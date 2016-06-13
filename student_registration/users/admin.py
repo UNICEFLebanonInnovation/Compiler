8b011ec77f376fc2dacc98e71e4f49892dd60f37
achamseddine <achamseddine@unicef.org>
2016-06-13 13:34:24 +0300

    filter outreach by owner

--- student_registration/users/admin.py
+++ student_registration/users/admin.py
@@ -9,30 +9,32 @@ from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import User
 
 
-class MyUserChangeForm(UserChangeForm):
-    class Meta(UserChangeForm.Meta):
-        model = User
+# class MyUserChangeForm(UserChangeForm):
+#     class Meta(UserChangeForm.Meta):
+#         model = User
+#
+#
+# class MyUserCreationForm(UserCreationForm):
+#
+#     error_message = UserCreationForm.error_messages.update({
+#         'duplicate_username': 'This username has already been taken.'
+#     })
+#
+#     class Meta(UserCreationForm.Meta):
+#         model = User
+#
+#     def clean_username(self):
+#         username = self.cleaned_data['username']
+#         try:
+#             User.objects.get(username=username)
+#         except User.DoesNotExist:
+#             return username
+#         raise forms.ValidationError(self.error_messages['duplicate_username'])
 
 
-class MyUserCreationForm(UserCreationForm):
+# @admin.register(User)
+# class UserAdmin(AuthUserAdmin):
+#     form = MyUserChangeForm
+#     add_form = MyUserCreationForm
 
-    error_message = UserCreationForm.error_messages.update({
-        'duplicate_username': 'This username has already been taken.'
-    })
-
-    class Meta(UserCreationForm.Meta):
-        model = User
-
-    def clean_username(self):
-        username = self.cleaned_data['username']
-        try:
-            User.objects.get(username=username)
-        except User.DoesNotExist:
-            return username
-        raise forms.ValidationError(self.error_messages['duplicate_username'])
-
-
-@admin.register(User)
-class UserAdmin(AuthUserAdmin):
-    form = MyUserChangeForm
-    add_form = MyUserCreationForm
+admin.site.register(User)
