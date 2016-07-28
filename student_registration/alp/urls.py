26888c4439be4642d5a5fa905865cbdbda91d439
achamseddine <achamseddine@unicef.org>
2016-07-28 16:04:49 +0300

    improving offline registring

--- student_registration/alp/urls.py
+++ student_registration/alp/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
         view=views.AttendanceView.as_view(),
         name='attendance'
     ),
-    url(regex=r'^outreach/export/$', view=views.ExportViewSet.as_view(), name='outreach_export'),
+    url(regex=r'^outreach/export/$', view=views.OutreachExportViewSet.as_view(), name='outreach_export'),
     url(regex=r'^registration/export/$', view=views.RegistrationExportViewSet.as_view(), name='registration_export'),
 ]
