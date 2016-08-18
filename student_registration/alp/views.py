ea30d0274ebbac9298eba10de017f0dbeef98b1d
achamseddine <achamseddine@unicef.org>
2016-08-18 10:00:38 +0300

    refactory student model into 2 apps

--- student_registration/alp/views.py
+++ student_registration/alp/views.py
@@ -13,22 +13,24 @@ from rest_framework import status
 from django.utils.translation import ugettext as _
 from import_export.formats import base_formats
 
-from .models import Outreach, ExtraColumn, Registration
-from .serializers import OutreachSerializer, ExtraColumnSerializer, RegistrationSerializer
+from .models import Outreach, ExtraColumn
+from .serializers import OutreachSerializer, ExtraColumnSerializer
+from student_registration.students.serializers import StudentSerializer
 from student_registration.students.models import (
     Student,
+    Language,
+    Nationality,
+)
+from student_registration.schools.models import (
     School,
     ClassRoom,
     Grade,
     Section,
-    Language,
     EducationLevel,
     ClassLevel,
-    Location,
-    Nationality,
     PartnerOrganization
 )
-from student_registration.students.serializers import StudentSerializer
+from student_registration.locations.models import Location
 
 
 class OutreachViewSet(mixins.RetrieveModelMixin,
@@ -109,49 +111,6 @@ class ExtraColumnViewSet(mixins.RetrieveModelMixin,
         return JsonResponse({'status': status.HTTP_200_OK})
 
 
-class RegistrationViewSet(mixins.RetrieveModelMixin,
-                      mixins.ListModelMixin,
-                      mixins.CreateModelMixin,
-                      mixins.UpdateModelMixin,
-                      viewsets.GenericViewSet):
-
-    model = Registration
-    queryset = Registration.objects.all()
-    serializer_class = RegistrationSerializer
-    permission_classes = (permissions.IsAuthenticated,)
-
-    def get_queryset(self):
-        if not self.request.user.is_superuser:
-            if self.request.user.school:
-                return self.queryset.filter(school=self.request.user.school.id)
-            else:
-                return []
-
-        return self.queryset
-
-    def create(self, request, *args, **kwargs):
-        """
-        :return: JSON
-        """
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.instance = serializer.save()
-
-        return JsonResponse({'status': status.HTTP_201_CREATED, 'data': serializer.data})
-
-    def delete(self, request, *args, **kwargs):
-        instance = self.model.objects.get(id=kwargs['pk'])
-        student = instance.student
-        instance.delete()
-        if student:
-            student.delete()
-        return JsonResponse({'status': status.HTTP_200_OK})
-
-    def update(self, request, *args, **kwargs):
-        instance = self.model.objects.get(id=kwargs['pk'])
-        return JsonResponse({'status': status.HTTP_200_OK})
-
-
 class OutreachView(LoginRequiredMixin, ListView):
     model = Outreach
     template_name = 'alp/outreach.html'
@@ -173,27 +132,6 @@ class OutreachView(LoginRequiredMixin, ListView):
             'nationalities': Nationality.objects.all(),
             'partners': PartnerOrganization.objects.all(),
             'distances': (u'<= 2.5km', u'> 2.5km', u'> 10km'),
-            'genders': (u'Male', u'Female'),
-        }
-
-
-class RegistrationView(LoginRequiredMixin, ListView):
-    model = Registration
-    template_name = 'alp/registration_list.html'
-
-    def get_context_data(self, **kwargs):
-        data = self.model.objects.all()
-        if not self.request.user.is_superuser:
-            data = data.filter(owner=self.request.user)
-            self.template_name = 'alp/registration.html'
-
-        return {
-            'registrations': data,
-            'classrooms': ClassRoom.objects.all(),
-            'schools': School.objects.all(),
-            'grades': Grade.objects.all(),
-            'sections': Section.objects.all(),
-            'nationalities': Nationality.objects.all(),
             'genders': (u'Male', u'Female'),
         }
 
@@ -274,58 +212,4 @@ class OutreachExportViewSet(LoginRequiredMixin, ListView):
             content_type='application/application/ms-excel',
         )
         response['Content-Disposition'] = 'attachment; filename=outreach_list.xls'
-        return response
-
-
-class RegistrationExportViewSet(LoginRequiredMixin, ListView):
-    model = Registration
-
-    def get(self, request, *args, **kwargs):
-
-        queryset = self.model.objects.all()
-
-        if not self.request.user.is_superuser:
-            queryset = queryset.filter(owner=self.request.user)
-
-        data = tablib.Dataset()
-        data.headers = [
-                    _('Student number'), _('Student fullname'), _('Mother fullname'), _('Nationality'),
-                    _('Day of birth'), _('Month of birth'), _('Year of birth'), _('Sex'),
-                    _('ID Number tooltip'), _('Phone number'), _('Student living address'),
-                    # _('Section'), _('Grade'),
-                    _('Class room'),
-                    _('School'), _('School number')
-        ]
-
-        content = []
-        for line in queryset:
-            # if not line.student or not line.grade or not line.section or not line.school:
-            if not line.student or not line.classroom or not line.school:
-                continue
-            content = [
-                '',
-                line.student.full_name,
-                line.student.mother_fullname,
-                line.student.nationality.name,
-                int(line.student.birthday_day),
-                int(line.student.birthday_month),
-                int(line.student.birthday_year),
-                _(line.student.sex),
-                line.student.id_number,
-                line.student.phone,
-                line.student.address,
-                line.classroom.name,
-                # line.section.name,
-                # line.grade.name,
-                line.school.name,
-                line.school.number
-            ]
-            data.append(content)
-
-        file_format = base_formats.XLS()
-        response = HttpResponse(
-            file_format.export_data(data),
-            content_type='application/application/ms-excel',
-        )
-        response['Content-Disposition'] = 'attachment; filename=registration_list.xls'
         return response
--- student_registration/registrations/serializers.py
+++ student_registration/registrations/serializers.py
@@ -1,15 +1,11 @@
 
 from rest_framework import serializers
-from .models import Outreach, ExtraColumn
+from .models import Registration
 from student_registration.students.serializers import StudentSerializer
 
 
-class ExtraColumnSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ExtraColumn
+class RegistrationSerializer(serializers.ModelSerializer):
 
-
-class OutreachSerializer(serializers.ModelSerializer):
     original_id = serializers.IntegerField(source='id', read_only=True)
     student_id = serializers.IntegerField(source='student.id', read_only=True)
     student_full_name = serializers.CharField(source='student.full_name')
@@ -22,23 +18,21 @@ class OutreachSerializer(serializers.ModelSerializer):
     student_id_number = serializers.CharField(source='student.id_number')
     student_nationality = serializers.CharField(source='student.nationality')
     student_address = serializers.CharField(source='student.address')
-    partner_name = serializers.CharField(source='partner.name', read_only=True)
     school_name = serializers.CharField(source='school.name', read_only=True)
     school_number = serializers.CharField(source='school.number', read_only=True)
-    location_name = serializers.CharField(source='location.name', read_only=True)
-    preferred_language_name = serializers.CharField(source='preferred_language.name', read_only=True)
-    last_education_level_name = serializers.CharField(source='last_education_level.name', read_only=True)
-    last_class_level_name = serializers.CharField(source='last_class_level.name', read_only=True)
+    section_name = serializers.CharField(source='section.name', read_only=True)
+    grade_name = serializers.CharField(source='grade.name', read_only=True)
+    classroom_name = serializers.CharField(source='classroom.name', read_only=True)
 
     def create(self, validated_data):
-
+        
         student_data = validated_data.pop('student', None)
         student_serializer = StudentSerializer(data=student_data)
         student_serializer.is_valid(raise_exception=True)
         student_serializer.instance = student_serializer.save()
 
         try:
-            instance = Outreach.objects.create(**validated_data)
+            instance = Registration.objects.create(**validated_data)
             instance.student = student_serializer.instance
             instance.save()
 
@@ -48,7 +42,7 @@ class OutreachSerializer(serializers.ModelSerializer):
         return instance
 
     class Meta:
-        model = Outreach
+        model = Registration
         fields = (
             'id',
             'original_id',
@@ -63,24 +57,14 @@ class OutreachSerializer(serializers.ModelSerializer):
             'student_id_number',
             'student_nationality',
             'student_address',
-            'partner',
-            'partner_name',
             'school',
             'school_name',
             'school_number',
-            'location',
-            'location_name',
-            'preferred_language',
-            'preferred_language_name',
-            'last_education_level',
-            'last_education_level_name',
-            'last_class_level',
-            'last_class_level_name',
-            'last_education_year',
-            'average_distance',
-            'exam_year',
-            'exam_month',
-            'exam_day',
+            'grade',
+            'grade_name',
+            'section',
+            'section_name',
+            'classroom',
+            'classroom_name',
             'owner',
-            # 'extra_fields',
         )
