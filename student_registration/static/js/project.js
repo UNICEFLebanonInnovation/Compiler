7a3d2fe0bf492011af5cee3bc61c1ea826dcaf54
achamseddine <achamseddine@unicef.org>
2016-08-31 14:59:43 +0300

    cleanup and optimization

--- student_registration/static/js/project.js
+++ student_registration/static/js/project.js
@@ -41,6 +41,16 @@ loseCode = function(str){
     return hash;
 };
 
+$('#radioBtn a').on('click', function(){
+    var sel = $(this).data('title');
+    var tog = $(this).data('toggle');
+    $('#'+tog).prop('value', sel);
+    $('#'+tog+'_'+sel).trigger('click');
+
+    $('a[data-toggle="'+tog+'"]').not('[data-title="'+sel+'"]').removeClass('active').addClass('notActive');
+    $('a[data-toggle="'+tog+'"][data-title="'+sel+'"]').removeClass('notActive').addClass('active');
+})
+
 
 function getCookie(name) {
     var cookieValue = null;
