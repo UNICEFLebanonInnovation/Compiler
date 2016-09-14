0310b7923b898a3c8a5295ea255f4fd568804aa3
ybaydoun <ybaydoun@hotmail.com>
2016-09-14 09:48:28 +0300

    Java script Validation

--- student_registration/static/js/validator.js
+++ student_registration/static/js/validator.js
@@ -7,49 +7,137 @@ var validation_mapping = new Array();
 function validateSection(frame, sectionIndex)
 {
     var result = true;
+
     if(sectionIndex.toString() in validation_mapping)
     {
         console.log(validation_mapping[sectionIndex]);
         var callback  = validation_mapping[sectionIndex];
         result = callback();
     }
-
-    //frame.next();
-
     return result;
-    // return false;
 }
 
 function validateSection5()
 {
+    var valid = true ;
     var selectedOption = $("#id_id_type").val();
-
     if(selectedOption == 1) {
-        $('#id_number_UNHCR_Other_error').show();
-        return false ;
+        if($('#id_id_number').val() == ""){
+            $('#id_number_UNHCR_Other_error').show();
+            valid = false ;
+        }else{
+            $('#id_number_UNHCR_Other_error').hide();
+        }
     }else if (selectedOption == 2 || selectedOption == 3 || selectedOption == 4|| selectedOption == 5) {
-        $('#id_number_UNHCR_Other_error').show();
-        $('#first_name_error').show();
-        $('#father_name_error').show();
-        $('#last_name_error').show();
-        $('#mother_fullname_error').show();
-        $('#age_error').show();
-        $('#gender_error').show();
-        $('#relationship_householdhead_error').show();
-        return false;
+
+        if($('#id_id_number').val() == ""){
+            $('#id_number_UNHCR_Other_error').show();
+            valid = false ;
+        }else{
+            $('#id_number_UNHCR_Other_error').hide();
+        }
+
+         if($('#id_first_name').val() == ""){
+            $('#first_name_error').show();
+            valid = false ;
+        }else{
+            $('#first_name_error').hide();
+        }
+
+         if($('#id_father_name').val() == ""){
+            $('#father_name_error').show();
+            valid = false ;
+        }else{
+            $('#father_name_error').hide();
+        }
+
+         if($('#id_last_name').val() == ""){
+            $('#last_name_error').show();
+            valid = false ;
+        }else{
+            $('#last_name_error').hide();
+        }
+
+         if($('#id_mother_fullname').val() == ""){
+            $('#mother_fullname_error').show();
+            valid = false ;
+        }else{
+            $('#mother_fullname_error').hide();
+        }
+
+         if($('#id_age').val() == ""){
+            $('#age_error').show();
+            valid = false ;
+        }else{
+            $('#age_error').hide();
+        }
+
+         if($('#id_sex').val() == ""){
+            $('#gender_error').show();
+            valid = false ;
+        }else{
+            $('#gender_error').hide();
+        }
+
+        if($('#id_relation_to_householdhead').val() == ""){
+            $('#relationship_householdhead_error').show();
+            valid = false ;
+        }else{
+            $('#relationship_householdhead_error').hide();
+        }
 
     }else if (selectedOption == 6) {
-        $('#first_name_error').show();
-        $('#father_name_error').show();
-        $('#last_name_error').show();
-        $('#mother_fullname_error').show();
-        $('#age_error').show();
-        $('#gender_error').show();
-        $('#relationship_householdhead_error').show();
-        return false;
-    }
+         if($('#id_first_name').val() == ""){
+            $('#first_name_error').show();
+            valid = false ;
+        }else{
+            $('#first_name_error').hide();
+        }
 
-    return true;
+         if($('#id_father_name').val() == ""){
+            $('#father_name_error').show();
+            valid = false ;
+        }else{
+            $('#father_name_error').hide();
+        }
+
+         if($('#id_last_name').val() == ""){
+            $('#last_name_error').show();
+            valid = false ;
+        }else{
+            $('#last_name_error').hide();
+        }
+
+         if($('#id_mother_fullname').val() == ""){
+            $('#mother_fullname_error').show();
+            valid = false ;
+        }else{
+            $('#mother_fullname_error').hide();
+        }
+
+         if($('#id_age').val() == ""){
+            $('#age_error').show();
+            valid = false ;
+        }else{
+            $('#age_error').hide();
+        }
+
+         if($('#id_sex').val() == ""){
+            $('#gender_error').show();
+            valid = false ;
+        }else{
+            $('#gender_error').hide();
+        }
+
+        if($('#id_relation_to_householdhead').val() == ""){
+            $('#relationship_householdhead_error').show();
+            valid = false ;
+        }else{
+            $('#relationship_householdhead_error').hide();
+        }
+    }
+   // alert(result);
+    return valid;
 }
 
 function validate_add_child_noid()
