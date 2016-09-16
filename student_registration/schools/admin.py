d135ebbefd3c86a2434656a9ac2a1e7062a7a923
ybaydoun <ybaydoun@hotmail.com>
2016-09-16 18:29:03 +0300

    User, and School Import Export from admin

--- student_registration/schools/admin.py
+++ student_registration/schools/admin.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
+
 from django.contrib import admin
+from import_export import resources, fields
+from import_export import fields
+from import_export.admin import ImportExportModelAdmin
 
 from .models import (
     School,
@@ -14,8 +18,23 @@ from .models import (
     PartnerOrganization,
 )
 
+class schoolResource(resources.ModelResource):
+    class Meta:
+        model = School
+        fields = (
+            'name',
+            'number',
+            'location',
+        )
+        export_order = ('name', )
 
-admin.site.register(School)
+
+class SchoolAdmin(ImportExportModelAdmin):
+    resource_class = schoolResource
+
+
+
+admin.site.register(School, SchoolAdmin)
 # admin.site.register(Course)
 admin.site.register(EducationLevel)
 admin.site.register(ClassLevel)
@@ -23,4 +42,12 @@ admin.site.register(Grade)
 admin.site.register(Section)
 admin.site.register(ClassRoom)
 admin.site.register(PartnerOrganization)
+
+
+
+
+
+
+
+
 
--- student_registration/users/admin.py
+++ student_registration/users/admin.py
@@ -5,8 +5,33 @@ from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from import_export import resources, fields
+from import_export import fields
+from import_export.admin import ImportExportModelAdmin
 
-from .models import User
+
+from .models import (
+    User,
+)
+
+class UserResource(resources.ModelResource):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'is_active',
+            'phone_number',
+            'school',
+            'location',
+        )
+        export_order = ('first_name', 'last_name')
+
+class UserAdmin(ImportExportModelAdmin):
+    resource_class = UserResource
+
+admin.site.register(User, UserAdmin)
 
 
-admin.site.register(User)
