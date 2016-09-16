fb598727f7873b96d8bd38b5968d28ab9511ed25
ybaydoun <ybaydoun@hotmail.com>
2016-09-16 13:28:32 +0300

    Validation enhancements

--- student_registration/static/js/validator.js
+++ student_registration/static/js/validator.js
@@ -34,135 +34,41 @@ function validateSection5()
     var valid = true ;
     var selectedOption = $("#id_id_type").val();
 
-    if($('#id_nationality').val() == ""){
-            $('#nationality_error').show();
-            valid = false ;
-        }else{
-            $('#nationality_error').hide();
-        }
-    if($('#id_id_type').val() == ""){
-            $('#id_type_error').show();
-            valid = false ;
-        }else{
-            $('#id_type_error').hide();
-        }
+    valid = validateTextBoxRequired('id_nationality','nationality_error',valid);
+    valid = validateTextBoxRequired('id_id_type','id_type_error',valid)
+
     if(selectedOption == 1) {
-        if($('#id_id_number').val() == ""){
-            $('#id_number_UNHCR_Other_error').show();
-            valid = false ;
-        }else{
-            $('#id_number_UNHCR_Other_error').hide();
-        }
+         valid = validateTextBoxRequired('id_id_number','id_number_UNHCR_Other_error',valid);
     }else if (selectedOption == 2 || selectedOption == 3 || selectedOption == 4|| selectedOption == 5) {
-
-        if($('#id_id_number').val() == ""){
-            $('#id_number_UNHCR_Other_error').show();
-            valid = false ;
-        }else{
-            $('#id_number_UNHCR_Other_error').hide();
-        }
-
-         if($('#id_first_name').val() == ""){
-            $('#first_name_error').show();
-            valid = false ;
-        }else{
-            $('#first_name_error').hide();
-        }
-
-        if($('#id_father_name').val() == ""){
-            $('#father_name_error').show();
-            valid = false ;
-        }else{
-            $('#father_name_error').hide();
-        }
-
-         if($('#id_last_name').val() == ""){
-            $('#last_name_error').show();
-            valid = false ;
-        }else{
-            $('#last_name_error').hide();
-        }
-
-         if($('#id_mother_fullname').val() == ""){
-            $('#mother_fullname_error').show();
-            valid = false ;
-        }else{
-            $('#mother_fullname_error').hide();
-        }
-
-         if($('#id_age').val() == ""){
-            $('#age_error').show();
-            valid = false ;
-        }else{
-            $('#age_error').hide();
-        }
-
-         if($('#id_sex').val() == ""){
-            $('#gender_error').show();
-            valid = false ;
-        }else{
-            $('#gender_error').hide();
-        }
-
-        if($('#id_relation_to_householdhead').val() == ""){
-            $('#relationship_householdhead_error').show();
-            valid = false ;
-        }else{
-            $('#relationship_householdhead_error').hide();
-        }
-
-
+        valid = validateTextBoxRequired('id_id_number','id_number_UNHCR_Other_error',valid);
+        valid = validateTextBoxRequired('id_first_name','first_name_error',valid);
+        valid = validateTextBoxRequired('id_father_name','father_name_error',valid);
+        valid = validateTextBoxRequired('id_last_name','last_name_error',valid);
+        valid = validateTextBoxRequired('id_mother_fullname','mother_fullname_error',valid);
+        valid = validateTextBoxRequired('id_age','age_error',valid);
+        valid = validateTextBoxRequired('id_sex','gender_error',valid);
+        valid = validateTextBoxRequired('id_relation_to_householdhead','relationship_householdhead_error',valid);
     }else if (selectedOption == 6) {
-         if($('#id_first_name').val() == ""){
-            $('#first_name_error').show();
-            valid = false ;
-        }else{
-            $('#first_name_error').hide();
-        }
-
-         if($('#id_father_name').val() == ""){
-            $('#father_name_error').show();
-            valid = false ;
-        }else{
-            $('#father_name_error').hide();
-        }
-
-         if($('#id_last_name').val() == ""){
-            $('#last_name_error').show();
-            valid = false ;
-        }else{
-            $('#last_name_error').hide();
-        }
-
-         if($('#id_mother_fullname').val() == ""){
-            $('#mother_fullname_error').show();
-            valid = false ;
-        }else{
-            $('#mother_fullname_error').hide();
-        }
-
-         if($('#id_age').val() == ""){
-            $('#age_error').show();
-            valid = false ;
-        }else{
-            $('#age_error').hide();
-        }
-
-         if($('#id_sex').val() == ""){
-            $('#gender_error').show();
-            valid = false ;
-        }else{
-            $('#gender_error').hide();
-        }
-
-        if($('#id_relation_to_householdhead').val() == ""){
-            $('#relationship_householdhead_error').show();
-            valid = false ;
-        }else{
-            $('#relationship_householdhead_error').hide();
-        }
+        valid = validateTextBoxRequired('id_first_name','first_name_error',valid);
+        valid = validateTextBoxRequired('id_father_name','father_name_error',valid);
+        valid = validateTextBoxRequired('id_last_name','last_name_error',valid);
+        valid = validateTextBoxRequired('id_mother_fullname','mother_fullname_error',valid);
+        valid = validateTextBoxRequired('id_age','age_error',valid);
+        valid = validateTextBoxRequired('id_sex','gender_error',valid);
+        valid = validateTextBoxRequired('id_relation_to_householdhead','relationship_householdhead_error',valid);
     }
-   // alert(result);
+    return valid;
+}
+
+function validateTextBoxRequired(id, errorID, isValid)
+{
+    var valid = isValid;
+    if($('#'+id).val() == ""){
+            $('#'+errorID).show();
+            valid = false ;
+        }else{
+            $('#'+errorID).hide();
+    }
     return valid;
 }
 
@@ -170,57 +76,13 @@ function validate_add_child_noid()
 {
     var valid = true ;
     var form = $('.bootbox-body').find('#add_child_noid_form');
-    // tr.append($('<td>').html(form.find('#id_school').find(':selected').text()));
-
-    if(form.find('#id_first_name').val() == ""){
-        form.find('#first_name_error').show();
-        valid = false ;
-    }else{
-        form.find('#first_name_error').hide();
-    }
-
-    if(form.find('#id_father_name').val() == ""){
-        form.find('#father_name_error').show();
-        valid = false ;
-    }else{
-        form.find('#father_name_error').hide();
-    }
-
-    if(form.find('#id_last_name').val() == ""){
-        form.find('#last_name_error').show();
-        valid = false ;
-    }else{
-        form.find('#last_name_error').hide();
-    }
-
-    if(form.find('#id_mother_fullname').val() == ""){
-        form.find('#mother_fullname_error').show();
-        valid = false ;
-    }else{
-        form.find('#mother_fullname_error').hide();
-    }
-
-    if(form.find('#id_age').val() == ""){
-        form.find('#age_error').show();
-        valid = false ;
-    }else{
-        form.find('#age_error').hide();
-    }
-
-    if(form.find('#id_sex').val() == ""){
-        form.find('#gender_error').show();
-        valid = false ;
-    }else{
-        form.find('#gender_error').hide();
-    }
-
-    if(form.find('#id_relation_to_adult').val() == ""){
-        form.find('#relation_to_adult_error').show();
-        valid = false ;
-    }else{
-        form.find('#relation_to_adult_error').hide();
-    }
-
+    valid = validateTextBox(form,'id_first_name','first_name_error',valid);
+    valid = validateTextBox(form,'id_father_name','father_name_error',valid);
+    valid = validateTextBox(form,'id_last_name','last_name_error',valid);
+    valid = validateTextBox(form,'id_mother_fullname','mother_fullname_error',valid);
+    valid = validateTextBox(form,'id_age','age_error',valid);
+    valid = validateTextBox(form,'id_sex','gender_error',valid);
+    valid = validateTextBox(form,'id_relation_to_adult','relation_to_adult_error',valid);
     return valid;
  }
 
@@ -228,16 +90,21 @@ function validate_add_child_noid()
 {
     var valid = true ;
     var form = $('.bootbox-body').find('#add_child_withid_form');
-
-    if(form.find('#id_relation_to_adult').val() == ""){
-        form.find('#relation_to_household_reprentative_error').show();
-        valid = false ;
-    }else{
-        form.find('#relation_to_household_reprentative_error').hide();
-    }
-     return valid;
+    valid = validateTextBox(form,'id_relation_to_adult','relation_to_household_reprentative_error',valid);
+    return valid;
 }
 
+ function validateTextBox(form,id, errorID, isValid)
+{
+    var valid = isValid;
+    if(form.find('#'+id).val() == ""){
+            form.find('#'+errorID).show();
+            valid = false ;
+        }else{
+            form.find('#'+errorID).hide();
+    }
+    return valid;
+}
 function checkArabicOnly(field)
 {
     var sNewVal = "";
