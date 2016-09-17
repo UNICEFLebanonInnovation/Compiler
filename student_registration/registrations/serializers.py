2987faf55d962989809221deab9f6c554e7e0194
achamseddine <achamseddine@unicef.org>
2016-09-17 23:41:07 +0300

    Registration child serializer, translation, and html fixes

--- student_registration/registrations/serializers.py
+++ student_registration/registrations/serializers.py
@@ -81,7 +81,25 @@ class RegistrationSerializer(serializers.ModelSerializer):
         )
 
 
-class RegistrationPilotSerializer(serializers.ModelSerializer):
+class RegistrationChildSerializer(serializers.ModelSerializer):
+
+    student_id = serializers.IntegerField(source='student.id', read_only=True)
+    first_name = serializers.CharField(source='student.first_name', read_only=True)
+    father_name = serializers.CharField(source='student.father_name', read_only=True)
+    last_name = serializers.CharField(source='student.last_name', read_only=True)
+    mother_fullname = serializers.CharField(source='student.mother_fullname', read_only=True)
+    sex = serializers.CharField(source='student.sex', read_only=True)
+    birthday_year = serializers.CharField(source='student.birthday_year', read_only=True)
+    birthday_month = serializers.CharField(source='student.birthday_month', read_only=True)
+    birthday_day = serializers.CharField(source='student.birthday_day', read_only=True)
+    id_number = serializers.CharField(source='student.id_number', read_only=True)
+    age = serializers.CharField(source='student.age', read_only=True)
+    relation_to_adult = serializers.CharField(source='student.relation_to_adult', read_only=True)
+    out_of_school_two_years = serializers.CharField(source='student.out_of_school_two_years', read_only=True)
+    related_to_family = serializers.CharField(source='student.related_to_family', read_only=True)
+    enrolled_last_year = serializers.CharField(source='student.enrolled_last_year', read_only=True)
+    enrolled_last_year_location = serializers.CharField(source='student.enrolled_last_year_location.id', read_only=True)
+    enrolled_last_year_school = serializers.CharField(source='student.enrolled_last_year_school.id', read_only=True)
 
     def create(self, validated_data):
 
@@ -92,7 +110,7 @@ class RegistrationPilotSerializer(serializers.ModelSerializer):
             instance.save()
 
         except Exception as ex:
-            raise serializers.ValidationError({'RegistrationPilot instance': ex.message})
+            raise serializers.ValidationError({'RegistrationChild instance': ex.message})
 
         return instance
 
@@ -105,12 +123,33 @@ class RegistrationPilotSerializer(serializers.ModelSerializer):
             'related_to_family',
             'enrolled_last_year_location',
             'enrolled_last_year',
+            'student_id',
+            'first_name',
+            'father_name',
+            'last_name',
+            'mother_fullname',
+            'sex',
+            'age',
+            'birthday_year',
+            'birthday_month',
+            'birthday_day',
+            'id_number',
+            'relation_to_adult',
+            'out_of_school_two_years',
+            'related_to_family',
+            'enrolled_last_year',
+            'enrolled_last_year_location',
+            'enrolled_last_year_school',
         )
 
 
 class RegisteringAdultSerializer(serializers.ModelSerializer):
 
     id = serializers.IntegerField(read_only=True)
+    children = RegistrationChildSerializer(many=True, read_only=True)
+
+    def get_children(self):
+        pass
 
     def create(self, validated_data):
 
@@ -155,4 +194,5 @@ class RegisteringAdultSerializer(serializers.ModelSerializer):
             'primary_phone_answered',
             'secondary_phone',
             'secondary_phone_answered',
+            'children',
         )
