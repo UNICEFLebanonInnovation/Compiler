b88bbe8f7cd8e274e62b2629e615f42c00129411
achamseddine <achamseddine@unicef.org>
2016-09-18 14:12:16 +0300

    Registration child view set

--- student_registration/registrations/serializers.py
+++ student_registration/registrations/serializers.py
@@ -84,27 +84,27 @@ class RegistrationSerializer(serializers.ModelSerializer):
 class RegistrationChildSerializer(serializers.ModelSerializer):
 
     student_id = serializers.IntegerField(source='student.id', read_only=True)
-    first_name = serializers.CharField(source='student.first_name', read_only=True)
-    father_name = serializers.CharField(source='student.father_name', read_only=True)
-    last_name = serializers.CharField(source='student.last_name', read_only=True)
-    mother_fullname = serializers.CharField(source='student.mother_fullname', read_only=True)
-    sex = serializers.CharField(source='student.sex', read_only=True)
-    birthday_year = serializers.CharField(source='student.birthday_year', read_only=True)
-    birthday_month = serializers.CharField(source='student.birthday_month', read_only=True)
-    birthday_day = serializers.CharField(source='student.birthday_day', read_only=True)
-    id_number = serializers.CharField(source='student.id_number', read_only=True)
-    age = serializers.CharField(source='student.age', read_only=True)
-    relation_to_adult = serializers.CharField(source='student.relation_to_adult', read_only=True)
-    out_of_school_two_years = serializers.CharField(source='student.out_of_school_two_years', read_only=True)
-    related_to_family = serializers.CharField(source='student.related_to_family', read_only=True)
-    enrolled_last_year = serializers.CharField(source='student.enrolled_last_year', read_only=True)
-    enrolled_last_year_location = serializers.CharField(source='student.enrolled_last_year_location.id', read_only=True)
-    enrolled_last_year_school = serializers.CharField(source='student.enrolled_last_year_school.id', read_only=True)
+    first_name = serializers.CharField(source='student.first_name')
+    father_name = serializers.CharField(source='student.father_name')
+    last_name = serializers.CharField(source='student.last_name')
+    mother_fullname = serializers.CharField(source='student.mother_fullname')
+    sex = serializers.CharField(source='student.sex')
+    birthday_year = serializers.CharField(source='student.birthday_year')
+    birthday_month = serializers.CharField(source='student.birthday_month')
+    birthday_day = serializers.CharField(source='student.birthday_day')
+    id_number = serializers.CharField(source='student.id_number')
+    age = serializers.CharField(source='student.age')
 
     def create(self, validated_data):
 
+        student_data = validated_data.pop('student', None)
+        student_serializer = StudentSerializer(data=student_data)
+        student_serializer.is_valid(raise_exception=True)
+        student_serializer.instance = student_serializer.save()
+
         try:
             instance = Registration.objects.create(**validated_data)
+            instance.student = student_serializer.instance
             if not instance.related_to_family:
                 instance.status = False
             instance.save()
@@ -123,6 +123,7 @@ class RegistrationChildSerializer(serializers.ModelSerializer):
             'related_to_family',
             'enrolled_last_year_location',
             'enrolled_last_year',
+            'registering_adult',
             'student_id',
             'first_name',
             'father_name',
@@ -134,12 +135,7 @@ class RegistrationChildSerializer(serializers.ModelSerializer):
             'birthday_month',
             'birthday_day',
             'id_number',
-            'relation_to_adult',
-            'out_of_school_two_years',
-            'related_to_family',
-            'enrolled_last_year',
-            'enrolled_last_year_location',
-            'enrolled_last_year_school',
+            'owner',
         )
 
 
@@ -188,6 +184,7 @@ class RegisteringAdultSerializer(serializers.ModelSerializer):
             'child_enrolled_in_other_schools',
             'previously_registered',
             'previously_registered_status',
+            'previously_registered_number',
             'signature',
             'address',
             'primary_phone',
