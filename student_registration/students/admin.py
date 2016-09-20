60ef4a767b21c76be4f70cc4c62398648878d7ae
ybaydoun <ybaydoun@hotmail.com>
2016-09-20 18:02:18 +0300

    admin Import Export
    Nationality - IDType

--- student_registration/students/admin.py
+++ student_registration/students/admin.py
@@ -2,6 +2,9 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.contrib import admin
+from import_export import resources, fields
+from import_export import fields
+from import_export.admin import ImportExportModelAdmin
 
 from .models import (
     Student,
@@ -10,8 +13,31 @@ from .models import (
     IDType,
 )
 
+class NationalityResource(resources.ModelResource):
+    class Meta:
+        model = Nationality
+        fields = (
+            'name',
+        )
+        export_order = ('name', )
+
+
+class NationalityAdmin(ImportExportModelAdmin):
+    resource_class = NationalityResource
+
+class IDTypeResource(resources.ModelResource):
+    class Meta:
+        model = IDType
+        fields = (
+            'name'
+        )
+        export_order = ('name', )
+
+class IDTypeAdmin(ImportExportModelAdmin):
+        resource_class = IDTypeResource
+
 
 admin.site.register(Student)
-admin.site.register(Nationality)
+admin.site.register(Nationality,NationalityAdmin)
 admin.site.register(Language)
-admin.site.register(IDType)
+admin.site.register(IDType,IDTypeAdmin)
