cdc0379c38fc96845d1954cc5af82ce83316e474
achamseddine <achamseddine@unicef.org>
2016-09-26 19:10:58 +0300

    Disable debug in production

--- config/settings/production.py
+++ config/settings/production.py
@@ -217,4 +217,4 @@ RAVEN_CONFIG = {
 # DEBUG
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = env.bool('DJANGO_DEBUG', True)
+DEBUG = env.bool('DJANGO_DEBUG', False)
