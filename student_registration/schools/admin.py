fd5306b3e22ab926d173809636bb12cb4206e261
ali-haidar-24 <ali.haidar@live.com>
2016-09-30 10:48:29 +0300

    export in Registrating Adult and in schools

--- student_registration/schools/admin.py
+++ student_registration/schools/admin.py
@@ -35,14 +35,101 @@ class SchoolAdmin(ImportExportModelAdmin):
     resource_class = schoolResource
 
 
+class EducationLevelResource(resources.ModelResource):
+    class Meta:
+        model = EducationLevel
+        fields = (
+            'id',
+            'name'
+        )
+        export_order = ('name',)
+
+
+class EducationLevelAdmin(ImportExportModelAdmin):
+    resource_class = EducationLevelResource
+
+
+class ClassLevelResource(resources.ModelResource):
+    class Meta:
+        model = ClassLevel
+        fields = (
+            'id',
+            'name'
+        )
+        export_order = ('name',)
+
+
+class ClassLevelAdmin(ImportExportModelAdmin):
+    resource_class = ClassLevelResource
+
+
+class GradeResource(resources.ModelResource):
+    class Meta:
+        model = Grade
+        fields = (
+            'id',
+            'name'
+        )
+        export_order = ('name',)
+
+
+class GradeAdmin(ImportExportModelAdmin):
+    resource_class = GradeResource
+
+
+class SectionResource(resources.ModelResource):
+    class Meta:
+        model = Section
+        fields = (
+            'id',
+            'name'
+        )
+        export_order = ('name',)
+
+
+class SectionAdmin(ImportExportModelAdmin):
+    resource_class = SectionResource
+
+
+class ClassRoomResource(resources.ModelResource):
+    class Meta:
+        model = ClassRoom
+        fields = (
+            'id',
+            'name',
+            'school',
+            'grade',
+            'section'
+        )
+        export_order = ('name',)
+
+
+class ClassRoomAdmin(ImportExportModelAdmin):
+    resource_class = ClassRoomResource
+
+
+class PartnerOrganizationResource(resources.ModelResource):
+    class Meta:
+        model = PartnerOrganization
+        fields = (
+            'id',
+            'name'
+        )
+        export_order = ('name',)
+
+
+class PartnerOrganizationAdmin(ImportExportModelAdmin):
+    resource_class = PartnerOrganizationResource
+
+
 admin.site.register(School, SchoolAdmin)
 # admin.site.register(Course)
-admin.site.register(EducationLevel)
-admin.site.register(ClassLevel)
-admin.site.register(Grade)
-admin.site.register(Section)
-admin.site.register(ClassRoom)
-admin.site.register(PartnerOrganization)
+admin.site.register(EducationLevel, EducationLevelAdmin)
+admin.site.register(ClassLevel, ClassLevelAdmin)
+admin.site.register(Grade, GradeAdmin)
+admin.site.register(Section, SectionAdmin)
+admin.site.register(ClassRoom, ClassRoomAdmin)
+admin.site.register(PartnerOrganization, PartnerOrganizationAdmin)
 
 
 
