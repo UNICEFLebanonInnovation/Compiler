e888ca81a14a3211d6e2cc19edd3ed3ee4883903
ybaydoun <ybaydoun@hotmail.com>
2016-10-25 14:54:47 +0300

    add card status

--- student_registration/registrations/models.py
+++ student_registration/registrations/models.py
@@ -50,6 +50,13 @@ class RegisteringAdult(Person):
         ('notrelay', _('Someone who may not relay the message to me')),
     )
 
+    CARD_STATUS = Choices(
+        ('notsent', _('Not sent yet')),
+        ('pending', _('Pending distribution')),
+        ('noshow', _('No show')),
+        ('distributed', _('Distributed')),
+    )
+
     individual_id_number = models.CharField(max_length=45L, blank=True, null=True)
     principal_applicant_living_in_house = models.BooleanField(blank=True, default=True)
     status = models.BooleanField(blank=True, default=True)
@@ -60,6 +67,7 @@ class RegisteringAdult(Person):
     wfp_case_number = models.CharField(max_length=50, blank=True, null=True)
     csc_case_number = models.CharField(max_length=50, blank=True, null=True)
     card_issue_requested = models.BooleanField(default=False)
+    card_status_distribution = models.CharField(max_length=50, blank=True, null=True, choices=CARD_STATUS)
     child_enrolled_in_this_school = models.PositiveIntegerField(blank=True, null=True)
     child_enrolled_in_other_schools = models.BooleanField(default=False)
     primary_phone = models.CharField(max_length=50, blank=True, null=True)
