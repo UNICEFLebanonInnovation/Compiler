259a37a9ce728e1306c8612343718c6258c94496
ybaydoun <ybaydoun@hotmail.com>
2016-11-04 08:47:11 +0200

    Household Visit list

--- student_registration/hhvisit/urls.py
+++ student_registration/hhvisit/urls.py
@@ -10,5 +10,12 @@ urlpatterns = [
         regex=r'^household-visit/$',
         view=views.HouseholdVisitView.as_view(),
         name='household_visit'
+    )
+    ,
+    url(
+        regex=r'^household-visit-list/$',
+        view=views.HouseholdVisitListView.as_view(),
+        name='household_visit_list'
     ),
 ]
+
