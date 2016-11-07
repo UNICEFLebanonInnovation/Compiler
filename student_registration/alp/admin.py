efd57c55ad75ed71d13dad95dd6b7d122eb1b9c8
achamseddine <achamseddine@unicef.org>
2016-11-07 21:35:32 +0200

    ALP CERD system
    ALP admin
    generate migration files
    note to float field
    Outreach serializer
    generate unique number

--- student_registration/alp/admin.py
+++ student_registration/alp/admin.py
@@ -2,6 +2,10 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.contrib import admin
+from import_export import resources, fields
+from import_export import fields
+from import_export.admin import ImportExportModelAdmin
+from import_export.widgets import *
 
 from .models import (
     Outreach,
@@ -9,5 +13,19 @@ from .models import (
 )
 
 
-admin.site.register(Outreach)
+class OutreachResource(resources.ModelResource):
+
+    class Meta:
+        model = Outreach
+
+
+class OutreachAdmin(ImportExportModelAdmin):
+    resource_class = OutreachResource
+    list_display = (
+        'student', 'student_age', 'school', 'registered_in_level', 'section'
+    )
+    list_filter = ('registered_in_level', 'school', 'location',)
+
+
+admin.site.register(Outreach, OutreachAdmin)
 admin.site.register(ALPRound)
