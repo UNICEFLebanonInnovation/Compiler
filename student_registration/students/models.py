009fa500c06bb9b7d2c66037f520fd15a0769422
achamseddine <achamseddine@unicef.org>
2016-11-09 14:21:42 +0200

    change month values, change ALP columns order

--- student_registration/students/models.py
+++ student_registration/students/models.py
@@ -46,18 +46,18 @@ class IDType(models.Model):
 class Person(TimeStampedModel):
 
     MONTHS = Choices(
-        (1, _('January')),
-        (2, _('February')),
-        (3, _('March')),
-        (4, _('April')),
-        (5, _('May')),
-        (6, _('June')),
-        (7, _('July')),
-        (8, _('August')),
-        (9, _('September')),
-        (10, _('October')),
-        (11, _('November')),
-        (12, _('December')),
+        ('1', _('January')),
+        ('2', _('February')),
+        ('3', _('March')),
+        ('4', _('April')),
+        ('5', _('May')),
+        ('6', _('June')),
+        ('7', _('July')),
+        ('8', _('August')),
+        ('9', _('September')),
+        ('10', _('October')),
+        ('11', _('November')),
+        ('12', _('December')),
     )
 
     GENDER = Choices(
