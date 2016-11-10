aa8a90654672ea0295050c1b33bd89b5f9c25184
achamseddine <achamseddine@unicef.org>
2016-11-10 16:06:15 +0200

    add ALP list filter

--- student_registration/alp/admin.py
+++ student_registration/alp/admin.py
@@ -22,10 +22,33 @@ class OutreachResource(resources.ModelResource):
 class OutreachAdmin(ImportExportModelAdmin):
     resource_class = OutreachResource
     list_display = (
-        'student', 'student_age', 'school', 'registered_in_level', 'section'
+        'student', 'student_age', 'school', 'caza', 'governorate',
+        'level', 'total', 'assigned_to_level', 'registered_in_level', 'section',
     )
-    list_filter = ('registered_in_level', 'school', 'location',)
+    list_filter = ('registered_in_level', 'level', 'assigned_to_level', 'school', 'school__location', )
+    search_fields = (
+        'student__first_name', 'student__father_name', 'student__last_name', 'student__mother_fullname',
+        'school__name', 'school__number', 'student__id_number', 'school__location__name', 'level__name',
+    )
 
+    def caza(self, obj):
+        if obj.school and obj.school.location:
+            return obj.school.location.name
+        return ''
+
+    def governorate(self, obj):
+        if obj.school and obj.school.location and obj.school.location.parent:
+            return obj.school.location.parent.name
+        return ''
+
+    def total(self, obj):
+        total = obj.exam_total
+        if obj.level and obj.level.note:
+            total = u'{}/{}'.format(
+                str(total),
+                str(obj.level.note)
+            )
+        return total
 
 admin.site.register(Outreach, OutreachAdmin)
 admin.site.register(ALPRound)
