2abedd4bd613f7f35b15a4860a5ac76e3fbe3d7f
jamescw <jcranwellward@gmail.com>
2016-11-14 21:48:02 +0200

    pad unique number to produce fixed length ids

--- student_registration/students/utils.py
+++ student_registration/students/utils.py
@@ -22,44 +22,33 @@ def generate_id(
     import hashlib
 
     try:
+        # concatenate name to form full name with no spaces
         full_name = u'{}{}{}'.format(first_name, father_name, last_name)
-        full_name_char_count = len(full_name)
-        mother_name_char_count = len(mother_full_name)
 
-        full_name_hash = int(hashlib.sha1(full_name.encode('UTF-8')).hexdigest(), 16) % 10000
-        # if len(str(full_name_hash)) < 4:
-        #     full_name_hash = int(hashlib.sha1(full_name.encode('UTF-8')).hexdigest(), 16) % 10001
-        # if len(str(full_name_hash)) < 4:
-        #     full_name_hash = int(hashlib.sha1(full_name.encode('UTF-8')).hexdigest(), 16) % 10002
-        # if len(str(full_name_hash)) < 4:
-        #     full_name_hash = int(hashlib.sha1(full_name.encode('UTF-8')).hexdigest(), 16) % 10005
-        # if len(str(full_name_hash)) < 4:
-        #     full_name_hash = int(hashlib.sha1(full_name.encode('UTF-8')).hexdigest(), 16) % 10007
+        # take the count of full name and zero pad to two digits
+        full_name_char_count = '{0:0>2}'.format(len(full_name))
 
-        mother_name_hash = int(hashlib.sha1(mother_full_name.encode('UTF-8')).hexdigest(), 16) % 10000
-        # if len(str(mother_name_hash)) < 4:
-        #     mother_name_hash = int(hashlib.sha1(mother_full_name.encode('UTF-8')).hexdigest(), 16) % 10001
-        # if len(str(mother_name_hash)) < 4:
-        #     mother_name_hash = int(hashlib.sha1(mother_full_name.encode('UTF-8')).hexdigest(), 16) % 10002
-        # if len(str(mother_name_hash)) < 4:
-        #     mother_name_hash = int(hashlib.sha1(mother_full_name.encode('UTF-8')).hexdigest(), 16) % 10005
-        # if len(str(mother_name_hash)) < 4:
-        #     mother_name_hash = int(hashlib.sha1(mother_full_name.encode('UTF-8')).hexdigest(), 16) % 10007
-        #
-        # if mother_name_char_count < 10 and not len(str(mother_name_hash)) > 4:
-        #     mother_name_char_count = "0"+str(mother_name_char_count)
-        #
-        # if full_name_char_count < 10 and not len(str(full_name_hash)) > 4:
-        #     full_name_char_count = "0"+str(full_name_char_count)
+        # take the count of mother name and zero pad to two digits
+        mother_name_char_count = '{0:0>2}'.format(len(mother_full_name))
 
+        # take the hash of fullname and convert to integer, zero padding to 4 digits
+        full_name_hash = '{0:0>4}'.format(int(hashlib.sha1(full_name.encode('UTF-8')).hexdigest(), 16) % 10000)
+
+        # take the hash of mother name and convert to integer, zero padding to 4 digits
+        mother_name_hash = '{0:0>4}'.format(int(hashlib.sha1(mother_full_name.encode('UTF-8')).hexdigest(), 16) % 10000)
+
+        # take the first character of the gender to denote sex
         gender_first_char = gender[:1]
 
-        return '{}{}{}{}{}'.format(
-            str(full_name_char_count),
-            str(mother_name_char_count),
-            str(full_name_hash),
-            str(mother_name_hash),
-            gender_first_char,
+        # arrange in order
+        result = '{fullname_char}{mothername_char}{fullname_hash}{mothername_hash}{gender_char}'.format(
+            fullname_char=full_name_char_count,
+            mothername_char=mother_name_char_count,
+            fullname_hash=full_name_hash,
+            mothername_hash=mother_name_hash,
+            gender_char=gender_first_char,
         )
+        return result
+
     except Exception as exp:
         return ''
