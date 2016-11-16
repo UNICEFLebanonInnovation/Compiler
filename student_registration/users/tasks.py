faa46af7f1f56accaab9bfdc87cc1fe2d9f651f7
achamseddine <achamseddine@unicef.org>
2016-11-16 10:57:29 +0200

    assign groups to users

--- student_registration/users/tasks.py
+++ student_registration/users/tasks.py
@@ -11,9 +11,27 @@ def generate_passwords(group):
     for user in users:
         try:
             password = user.username
-            if user.phone_number:
-                password = password+user.phone_number[:2]
+            # if user.phone_number:
+            #     password = password+user.phone_number[:2]
+            if user.email:
+                password = password+user.email[:2]
             user.update_password(password)
+            user.save()
+        except Exception as ex:
+            print ex
+            pass
+
+
+@app.task
+def assign_group(group):
+    from student_registration.users.models import User
+
+    users = User.objects.filter(first_name__in=['school', 'director'])
+    # users = User.objects.filter(first_name__in=['school']).exclude(groups__id=group)
+    print len(users)
+    for user in users:
+        try:
+            user.groups.add(group)
             user.save()
         except Exception as ex:
             print ex
