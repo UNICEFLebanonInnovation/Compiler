cd1de2f1ae9002c252c676f17280c9c9d8db37d9
achamseddine <achamseddine@unicef.org>
2016-12-03 14:01:10 +0200

    setup dashboard v1

--- config/urls.py
+++ config/urls.py
@@ -77,6 +77,7 @@ urlpatterns = [
     url(r'^enrollments/', include('student_registration.enrollments.urls', namespace='enrollments')),
     url(r'^schools/', include('student_registration.schools.urls', namespace='schools')),
     url(r'^locations/', include('student_registration.locations.urls', namespace='locations')),
+    url(r'^dashboard/', include('student_registration.dashboard.urls', namespace='dashboard')),
 
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/docs/', include('rest_framework_swagger.urls')),
