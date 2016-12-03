5758cb2edd11dd58478b419de0aa07d2022dd214
achamseddine <achamseddine@unicef.org>
2016-12-03 18:42:42 +0200

    2nd shift and ALP dashboard

--- student_registration/dashboard/views.py
+++ student_registration/dashboard/views.py
@@ -111,7 +111,7 @@ class Registrations2ndShiftView(LoginRequiredMixin,
     """
     Provides the registration page with lookup types in the context
     """
-    model = Registration
+    model = Enrollment
     template_name = 'dashboard/registrations-2ndshift.html'
 
     # group_required = [u"editors", u"admins"]
@@ -136,7 +136,7 @@ class Registrations2ndShiftView(LoginRequiredMixin,
             # get number of children of each of these schools
             numchildren = 0
             for oneschool in govdistschools:
-                nbr = Enrollment.objects.filter(school=oneschool.id).count()
+                nbr = self.model.objects.filter(school=oneschool.id).count()
                 if nbr:
                     students_per_school[oneschool.name] = nbr
                 numchildren += nbr
@@ -146,28 +146,27 @@ class Registrations2ndShiftView(LoginRequiredMixin,
         # get children by age range
         now = datetime.datetime.now()
         age_range = {}
-        age_range['0-5'] = Student.objects.filter(birthday_year__gte= (now.year - 5)).count()
-        age_range['6-9'] = Student.objects.filter(birthday_year__lte= (now.year - 6), birthday_year__gte=(now.year - 9)).count()
-        age_range['10+'] = Student.objects.filter(birthday_year__lte=(now.year - 10)).count()
+        age_range['0-5'] = self.model.objects.filter(student__birthday_year__gte=(now.year - 5)).count()
+        age_range['6-9'] = self.model.objects.filter(student__birthday_year__lte=(now.year - 6), student__birthday_year__gte=(now.year - 9)).count()
+        age_range['10+'] = self.model.objects.filter(student__birthday_year__lte=(now.year - 10)).count()
 
         # get HHs by ID Type
         students_by_idtype = {}
         id_types = IDType.objects.all()
         for type in id_types:
-            students_by_idtype[type] = Enrollment.objects.filter(student__id_type=type).count()
+            students_by_idtype[type] = self.model.objects.filter(student__id_type=type).count()
 
         # get HHs by Nationality
         students_by_nationality = {}
         nationalities = Nationality.objects.all()
         for nationality in nationalities:
-            students_by_nationality[nationality] = Enrollment.objects.filter(student__nationality_id=nationality.id).count()
+            students_by_nationality[nationality] = self.model.objects.filter(student__nationality_id=nationality.id).count()
 
         return {
-                'schools': School.objects.count(),
-                'students': Student.objects.count(),
-                'registrations': Enrollment.objects.count(),
-                'males': Student.objects.filter(sex='Male').count(),
-                'females': Student.objects.filter(sex='Female').count(),
+                'schools': len(students_per_school),
+                'registrations': self.model.objects.count(),
+                'males': self.model.objects.filter(student__sex='Male').count(),
+                'females': self.model.objects.filter(student__sex='Female').count(),
                 'students_per_gov': students_per_gov,
                 'students_per_school': students_per_school,
                 'age_range': age_range,
@@ -183,8 +182,8 @@ class RegistrationsALPView(LoginRequiredMixin,
     """
     Provides the registration page with lookup types in the context
     """
-    model = Registration
-    template_name = 'dashboard/registrations-pilot.html'
+    model = Outreach
+    template_name = 'dashboard/registrations-alp.html'
 
     # group_required = [u"editors", u"admins"]
 
@@ -195,64 +194,56 @@ class RegistrationsALPView(LoginRequiredMixin,
 
     def get_context_data(self, **kwargs):
         # children by governate || get the governettes and get the number of children for each, and put them in a dictionary
-        # Also Adults by governate
         # Also schools by governate
-        governates = Location.objects.exclude(parent__isnull=False)
+        governorates = Location.objects.exclude(parent__isnull=False)
 
-        childrenPerGov = {}
-        adultsPerGov = {}
-        schoolsPerGov = {}
-        for gov in governates:
+        students_per_gov = {}
+        schools_per_gov = {}
+        students_per_school = {}
+        for gov in governorates:
             # get schools of this governate and its districts
             govdistschools = School.objects.filter(Q(location__parent__id=gov.id) | Q(location=gov.id))
-            schoolsPerGov[gov.name] = govdistschools.count()
+            schools_per_gov[gov.name] = govdistschools.count()
             # get number of children of each of these schools
             numchildren = 0
-            numadults = 0
             for oneschool in govdistschools:
-                numchildren = numchildren + Registration.objects.filter(school=oneschool.id).count()
-                numadults = numadults + RegisteringAdult.objects.filter(school=oneschool.id).count()
+                nbr = self.model.objects.filter(school=oneschool.id).count()
+                if nbr:
+                    students_per_school[oneschool.name] = nbr
+                numchildren += nbr
 
-            childrenPerGov[gov.name] = numchildren
-            adultsPerGov[gov.name] = numadults
-
-        # some students are not tied to any school, show them also in the charts
-        childrenPerGov['N/A'] = Registration.objects.filter(school__isnull=True).count()
-        adultsPerGov['N/A'] = RegisteringAdult.objects.filter(school__isnull=True).count()
+            students_per_gov[gov.name] = numchildren
 
         # get children by age range
         now = datetime.datetime.now()
-        agerange = {}
-        agerange['0-5'] = Student.objects.filter(birthday_year__gte= (now.year - 5)).count()
-        agerange['6-9'] = Student.objects.filter(birthday_year__lte= (now.year - 6), birthday_year__gte=(now.year - 9)).count()
-        agerange['10+'] = Student.objects.filter(birthday_year__lte=(now.year - 10)).count()
+        age_range = {}
+        age_range['0-5'] = self.model.objects.filter(student__birthday_year__gte=(now.year - 5)).count()
+        age_range['6-9'] = self.model.objects.filter(student__birthday_year__lte=(now.year - 6), student__birthday_year__gte=(now.year - 9)).count()
+        age_range['10+'] = self.model.objects.filter(student__birthday_year__lte=(now.year - 10)).count()
 
         # get HHs by ID Type
-        adultsbyidtype = {}
-        idtypes = IDType.objects.all()
-        for type in idtypes:
-            adultsbyidtype[type] = RegisteringAdult.objects.filter(id_type=type).count()
+        students_by_idtype = {}
+        id_types = IDType.objects.all()
+        for type in id_types:
+            students_by_idtype[type] = self.model.objects.filter(student__id_type=type).count()
 
         # get HHs by Nationality
-        adultsbynationality = {}
+        students_by_nationality = {}
         nationalities = Nationality.objects.all()
         for nationality in nationalities:
-            adultsbynationality[nationality] = RegisteringAdult.objects.filter(nationality=nationality).count()
+            students_by_nationality[nationality] = self.model.objects.filter(student__nationality_id=nationality.id).count()
 
         return {
-                'schools': School.objects.count(),
-                'students': Student.objects.count(),
-                'registrations': Registration.objects.count(),
-                'adults': RegisteringAdult.objects.count(),
-                'males': Student.objects.filter(sex='Male').count(),
-                'females': Student.objects.filter(sex='Female').count(),
-                'notenrolledlastyear': Registration.objects.filter(enrolled_last_year_school__isnull = True).count(),
-                'childrenPerGov': childrenPerGov,
-                'adultsPerGov': adultsPerGov,
-                'agerange': agerange,
-                'adultsbyidtype': adultsbyidtype,
-                'adultsbynationality': adultsbynationality,
-                'schoolsPerGov': schoolsPerGov,
+                'schools': len(students_per_school),
+                'registrations': self.model.objects.count(),
+                'males': self.model.objects.filter(student__sex='Male').count(),
+                'females': self.model.objects.filter(student__sex='Female').count(),
+                'students_per_gov': students_per_gov,
+                'students_per_school': students_per_school,
+                'age_range': age_range,
+                'students_by_idtype': students_by_idtype,
+                'students_by_nationality': students_by_nationality,
+                'schools_per_gov': schools_per_gov,
         }
 
 
