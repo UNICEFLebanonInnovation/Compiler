df49505a7ed478187632a6038688413b837dbdec
achamseddine <achamseddine@unicef.org>
2016-12-10 11:24:02 +0200

    enrolments dashboard and export

--- student_registration/enrollments/urls.py
+++ student_registration/enrollments/urls.py
@@ -16,5 +16,14 @@ urlpatterns = [
         view=views.EnrollmentStaffView.as_view(),
         name='enrollment_view'
     ),
-    url(regex=r'^enrollment/export/$', view=views.ExportViewSet.as_view(), name='enrollment_export'),
+    url(
+        regex=r'^enrollment/export/$',
+        view=views.ExportViewSet.as_view(),
+        name='enrollment_export'
+    ),
+    url(
+        regex=r'^enrollment/enrollment-export-by-school/$',
+        view=views.ExportBySchoolView.as_view(),
+        name='enrollment_export_by_school'
+    ),
 ]
