ee53e65b67c83e087747699fe740488c66bf2417
achamseddine <achamseddine@unicef.org>
2017-01-05 18:19:31 +0200

    push attendances for ALP

--- student_registration/attendances/management/commands/push_attendances_alp.py
+++ student_registration/attendances/management/commands/push_attendances_alp.py
@@ -8,5 +8,13 @@ from student_registration.attendances.tasks import *
 class Command(BaseCommand):
     help = 'Push attendance ALP data to CouchBase'
 
+    def add_arguments(self, parser):
+        parser.add_argument('schools', nargs='+', type=str)
+
     def handle(self, *args, **options):
-        set_app_attendances_alp()
+
+        if 'all' in options['schools']:
+            set_app_attendances_alp()
+        else:
+            for school in options['schools']:
+                set_app_attendances_alp(school)
