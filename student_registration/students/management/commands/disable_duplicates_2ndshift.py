473c4186dfb4febe5518f1cc2cc7d5978fe6c03a
achamseddine <achamseddine@unicef.org>
2017-01-07 15:32:22 +0200

    add offset for disable dups enrolment

--- student_registration/students/management/commands/disable_duplicates_2ndshift.py
+++ student_registration/students/management/commands/disable_duplicates_2ndshift.py
@@ -8,5 +8,9 @@ from student_registration.students.tasks import *
 class Command(BaseCommand):
     help = 'Disable duplicate enrolments'
 
+    def add_arguments(self, parser):
+        parser.add_argument('offset', nargs='+', type=int)
+
     def handle(self, *args, **options):
-        disable_duplicate_enrolments()
+        for offset in options['offset']:
+            disable_duplicate_enrolments(offset)
