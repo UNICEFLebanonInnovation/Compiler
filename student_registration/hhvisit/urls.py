0b01b3e93201b1298df3b96b68f76589b3aba933
ybaydoun <ybaydoun@hotmail.com>
2017-01-20 16:38:22 +0200

    Household enhancement

--- student_registration/hhvisit/urls.py
+++ student_registration/hhvisit/urls.py
@@ -32,6 +32,11 @@ urlpatterns = [
         view=views.LoadAbsences,
         name='household_visit_load_absences'
     ),
+    url(
+        regex=r'^save-absences/$',
+        view=views.SaveAbsences,
+        name='household_visit_save_absences'
+    ),
      url(
          regex=r'^student-absence/$',
          view=views.StudentAbsenceView.as_view(),
