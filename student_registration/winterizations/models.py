0a93d7e89fa4cb756c9d85bacb374d984cb9c19b
achamseddine <achamseddine@unicef.org>
2017-01-23 18:14:48 +0200

    add fields to winter model

--- student_registration/winterizations/models.py
+++ student_registration/winterizations/models.py
@@ -1,5 +1,66 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from model_utils import Choices
 
-# Create your models here.
+
+class Beneficiary(models.Model):
+
+    REGISTRATION_STATUS = Choices(
+        ('Registered', 'Registered'),
+        ('Recorded', 'Recorded'),
+        ('Unregistered', 'Unregistered'),
+    )
+    LOCATION_TYPE = Choices(
+        ('CS', 'CS'),
+        ('IS', 'IS'),
+    )
+    CARD_DISTRIBUTION_STATUS = Choices(
+        ('Distributed', 'Distributed'),
+        ('Not Distributed', 'Not Distributed'),
+    )
+    CARD_LOADED = Choices(
+        ('Yes', 'Yes'),
+        ('No', 'No'),
+    )
+
+    case_number = models.CharField(max_length=45L, unique=True)
+    registration_status = models.CharField(
+        max_length=45L,
+        blank=True,
+        null=True,
+        choices=REGISTRATION_STATUS
+    )
+    location_type = models.CharField(
+        max_length=45L,
+        blank=True,
+        null=True,
+        choices=LOCATION_TYPE
+    )
+    governorate = models.CharField(max_length=45L)
+    district = models.CharField(max_length=45L)
+    cadastral = models.CharField(max_length=45L)
+    phone_number = models.CharField(max_length=45L)
+    total_children = models.IntegerField()
+    card_distributed = models.CharField(
+        max_length=45L,
+        blank=True,
+        null=True,
+        choices=CARD_DISTRIBUTION_STATUS
+    )
+    card_loaded = models.CharField(
+        max_length=45L,
+        blank=True,
+        null=True,
+        choices=CARD_LOADED
+    )
+
+    class Meta:
+        ordering = ['case_number']
+        verbose_name_plural = "Beneficiaries"
+
+    def amount(self):
+        return self.total_children * 40
+
+    def __unicode__(self):
+        return self.case_number
