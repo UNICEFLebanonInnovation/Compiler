ded683d38af991d182d0c3023b7e51504c04ec87
James Cranwell-Ward <jcranwellward@gmail.com>
2017-01-29 19:38:12 +0200

    Winter helpdesk (#156)
    * create winter app
    
    * install plugins
    
    * update plugins
    
    * add fields to winter model
    
    * add filters to winter admin
    
    * overwrite helpdesk admin
    
    * App rename
    
    * rollback api and rest modul

--- config/settings/common.py
+++ config/settings/common.py
@@ -41,6 +41,9 @@ DJANGO_APPS = (
     # Admin
     'suit',
     'django.contrib.admin',
+    'markdown_deux',  # Required for Knowledgebase item formatting
+    'bootstrapform',  # Required for nicer formatting of forms with the default templates
+    'helpdesk',  # This is us!
 )
 THIRD_PARTY_APPS = (
     'crispy_forms',  # Form layouts
@@ -67,6 +70,8 @@ LOCAL_APPS = (
     'student_registration.eav',  # custom EAV app
     'student_registration.hhvisit',  # custom HH Visit app
     'student_registration.dashboard',  # custom dashboard app
+    'student_registration.helpdesks',  # custom helpdesk app
+    'student_registration.winterization',  # custom winterization app
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
@@ -317,6 +322,7 @@ SUIT_CONFIG = {
 
     'MENU': (
         {'label': 'View site', 'icon': 'icon-home', 'url': "/"},
+        {'app': 'helpdesk', 'label': 'Helpdesk Config', 'icon': 'icon-info-sign'},
         {'label': 'Dashboard', 'icon': 'icon-home', 'models': [
             {'url': '/dashboard/registrations-pilot/', 'label': 'PILOT'},
             {'url': '/dashboard/registrations-2ndshift/', 'label': '2nd Shift'},
@@ -331,10 +337,15 @@ SUIT_CONFIG = {
         {'app': 'schools', 'label': 'Schools', 'icon': 'icon-th-list'},
         {'app': 'hhvisit', 'label': 'House Hold Visit', 'icon': 'icon-th-list'},
         {'app': 'attendances', 'label': 'Attendances', 'icon': 'icon-th-list'},
+        {'app': 'winterization', 'label': 'Winterization', 'icon': 'icon-th-list'},
         {'app': 'locations', 'label': 'Locations', 'icon': 'icon-globe'},
     )
 }
 
+HELPDESK_TRANSLATE_TICKET_COMMENTS = True
+HELPDESK_SHOW_DELETE_BUTTON_SUPERUSER_FOLLOW_UP = True
+HELPDESK_STAFF_ONLY_TICKET_OWNERS = True
+HELPDESK_STAFF_ONLY_TICKET_CC = True
 
 ABSENCE_URL = env('ABSENCE_URL', default='NO URL for absentees set in the environment')
 ABSENCE_TOKEN = env('ABSENCE_TOKEN', default='No token for absentees URL set')
