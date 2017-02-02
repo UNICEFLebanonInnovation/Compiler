89a7ed17e6575b257e7eba0bb3f875fd4c510d64
James Cranwell-Ward <jcranwellward@gmail.com>
2017-02-02 13:14:52 +0200

    Phase 1: leverage mongo DB for aggregation

--- student_registration/attendances/mongo/flattern_attendnace_data.js
+++ student_registration/attendances/mongo/flattern_attendnace_data.js
@@ -1,43 +1,43 @@
-
 var map_function = function () {
 
-        var lookup = {};
-        for (var index in this.students) {
-            student = this.students[index];
-            lookup[student.student_id] = student;
+    var lookup = {};
+    for (var index in this.students) {
+        student = this.students[index];
+        lookup[student.student_id] = student;
+    }
+
+    for (var date in this.attendance) {
+        day = this.attendance[date];
+        for (var student in day['students']) {
+
+            var key = this.school + '-' + date + '-' + student;
+            record = day['students'][student];
+
+            var gender = ((lookup.hasOwnProperty(student)) ? lookup[student].gender : null);
+
+            var doc = {
+                date: new Date(date.split("-").reverse().join("-")),
+                school: this.school,
+                class: this.class_id,
+                section: this.section_id,
+                location: this.location_id,
+                student: student,
+                gender: gender,
+                attended: record.status,
+                reason: record.reason,
+                validation_date: ((day.hasOwnProperty('validation_date')) ? new Date(day.validation_date.split("-").reverse().join("-")) : null)
+
+            };
+            emit(key, doc);
         }
-
-        for (var date in this.attendance) {
-            day = this.attendance[date];
-            for (var student in day['students']) {
-
-                var key = this.school + '-' + date + '-' + student;
-                record = day['students'][student];
-
-                var gender = ((lookup.hasOwnProperty(student)) ? lookup[student].gender : null);
-
-                var doc = {
-                    date: date,
-                    school: this.school,
-                    class: this.class_id,
-                    section: this.section_id,
-                    location: this.location_id,
-                    student: student,
-                    gender: gender,
-                    attended: record.status,
-                    reason: record.reason,
-                    validation_date: day['validation_date']
-                };
-                emit(key, doc);
-            }
-        }
-    };
+    }
+};
 
 
 db.runCommand({
     'mapReduce': 'attendances',
     'map': map_function,
-    'reduce': function(key, values) {
+    'reduce': function (key, values) {
         return values[0];
     },
     'out': {replace: 'attendances_by_day'}
