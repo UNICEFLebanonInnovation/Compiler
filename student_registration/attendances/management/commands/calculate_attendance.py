e9a2e82a3ca0ccc678be3896e5f0745aca4e3b0f
James Cranwell-Ward <jcranwellward@gmail.com>
2017-02-03 17:29:53 +0200

    Calculation phase

--- student_registration/attendances/management/commands/calculate_attendance.py
+++ student_registration/attendances/management/commands/calculate_attendance.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand
 
-from student_registration.attendances.tasks import calculate_attendance_and_absentees
+from student_registration.attendances.tasks import calculate_attendance
 
 
 class Command(BaseCommand):
     help = 'Calculate attendances and absentees'
 
     def handle(self, *args, **options):
-        calculate_attendance_and_absentees()
+        calculate_attendance()
