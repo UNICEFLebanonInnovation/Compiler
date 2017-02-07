438cb76d706030aae39ac91c8b82d2f80d869d0b
James Cranwell-Ward <jcranwellward@gmail.com>
2017-02-07 11:58:38 +0200

    Include aggregate

--- student_registration/attendances/management/commands/calculate_attendance.py
+++ student_registration/attendances/management/commands/calculate_attendance.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 
 from student_registration.attendances.tasks import (
+    aggregate_attendace,
     calculate_by_day_summary
 )
 
@@ -9,4 +10,5 @@ class Command(BaseCommand):
     help = 'Calculate attendance'
 
     def handle(self, *args, **options):
+        aggregate_attendace()
         calculate_by_day_summary()
