79c0a9f36398ac673e82c4ec972b28b87077e5f0
ybaydoun <ybaydoun@hotmail.com>
2017-03-07 16:03:47 +0200

    Add serializer for not found

--- student_registration/registrations/models.py
+++ student_registration/registrations/models.py
@@ -191,6 +191,18 @@ class RegisteringAdult(Person):
         return self.first_name + ' ' + self.father_name + ' ' + self.last_name
 
 
+class HouseholdNotFound(Person):
+
+    number_children_five_to_nine = models.IntegerField(blank=True, null=True)
+    number_children_ten_to_seventeen = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['id']
+
+    def __unicode__(self):
+        return self.id
+
+
 class Complaint(TimeStampedModel):
     """
     Household complaints by hotline
@@ -208,7 +220,12 @@ class Complaint(TimeStampedModel):
     complaint_category = models.ForeignKey(
         ComplaintCategory,
         blank=True, null=True,
-        related_name='+',
+        related_name='complaints',
+    )
+    household_not_found = models.ForeignKey(
+        HouseholdNotFound,
+        blank=True, null=True,
+        related_name='HHNotFound',
     )
     complaint_note = models.TextField(blank=True, null=True)
     complaint_status = models.CharField(max_length=20, blank=True, null=True, choices=STATUS)
@@ -237,23 +254,6 @@ class Complaint(TimeStampedModel):
         return self.id
 
 
-class HouseholdNotFound(Person):
-
-    complaint = models.ForeignKey(
-        Complaint,
-        blank=True, null=True,
-        related_name='+',
-    )
-    number_children_five_to_nine = models.IntegerField(blank=True, null=True)
-    number_children_ten_to_seventeen = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        ordering = ['id']
-
-    def __unicode__(self):
-        return self.id
-
-
 class Payment(models.Model):
     """
     Monthly household payment in PILOT
@@ -271,7 +271,6 @@ class Payment(models.Model):
 
     class Meta:
         ordering = ['id']
-
 
     def __unicode__(self):
         return self.id
