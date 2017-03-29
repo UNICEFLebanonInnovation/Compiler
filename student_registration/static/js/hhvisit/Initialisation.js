d8faeb230b35e1c455f420fd6fd014327aebc238
ybaydoun <ybaydoun@hotmail.com>
2017-03-29 11:07:47 +0300

    Fixes

--- student_registration/static/js/hhvisit/Initialisation.js
+++ student_registration/static/js/hhvisit/Initialisation.js
@@ -30,7 +30,7 @@ var  visit_status = null;
                {
                   block = $(this);
 
-                  var visitID = block.attr('visitid')
+                  var visitID = block.attr('visitID')
 
                   $("#hhList").hide();
 
