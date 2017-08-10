872671114bc437f62026e5b4945bd67ecafb67b0
achamseddine <achamseddine@unicef.org>
2017-08-10 16:03:25 +0300

    Add enrollment list page

---
+++ student_registration/enrollments/filters.py
@@ -0,0 +1,12 @@
+from django_filters import FilterSet
+
+from .models import Enrollment
+
+
+class EnrollmentFilter(FilterSet):
+    class Meta:
+        model = Enrollment
+        fields = {
+            'student__first_name': ['contains'],
+            'student__id_number': ['exact'],
+        }
