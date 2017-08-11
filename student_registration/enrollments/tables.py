b214b7fade59724e354e60188bbf02b152af3300
achamseddine <achamseddine@unicef.org>
2017-08-11 10:48:52 +0300

    Add fields to the table 2

--- student_registration/enrollments/tables.py
+++ student_registration/enrollments/tables.py
@@ -10,18 +10,42 @@ class BootstrapTable(tables.Table):
         model = Enrollment
         template = 'django_tables2/bootstrap.html'
         attrs = {'class': 'table table-bordered table-striped table-hover'}
-        # exclude = ('friendly', )
 
 
 class EnrollmentTable(tables.Table):
 
-    student = tables.RelatedLinkColumn()
+    # student = tables.RelatedLinkColumn()
+    id = tables.LinkColumn()
 
     class Meta:
         model = Enrollment
         template = 'django_tables2/bootstrap.html'
-        # sequence = (
-        #     'student_fullname',
-        #     'student__father_name',
-        #     'student__last_name',
-        # )
+        fields = (
+            'id',
+            'student.first_name',
+            'student.father_name',
+            'student.last_name',
+            'student.sex',
+            'student.age',
+            'student.birthday',
+            'student.nationality',
+            'student.mother_fullname',
+            'student.mother_nationality',
+            'student.registered_in_unhcr',
+            'student.id_type',
+            'student.id_number',
+            'student.address',
+            'student.phone_number',
+            'classroom',
+            'section',
+            'last_education_level',
+            'last_school_type',
+            'last_school_shift',
+            'last_school',
+            'last_education_year',
+            'last_year_result',
+            'participated_in_alp',
+            'last_informal_edu_level',
+            'last_informal_edu_round',
+            'last_informal_edu_final_result',
+        )
