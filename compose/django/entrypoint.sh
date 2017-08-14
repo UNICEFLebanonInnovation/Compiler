7d23a34a437933465662201b1ba4dfa67201b162
James Cranwell-Ward <jcranwellward@gmail.com>
2017-08-14 23:21:49 +0300

    Add support for ssh on azure app service linux

--- compose/django/entrypoint.sh
+++ compose/django/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+touch /home/LogFiles/node_${WEBSITE_ROLE_INSTANCE_ID}_out.log
+echo "$(date) Container started" >> /home/LogFiles/node_${WEBSITE_ROLE_INSTANCE_ID}_out.log
+
 until psql $DATABASE_URL -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
