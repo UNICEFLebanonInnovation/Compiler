3367cc27c1481c85a83c27e70c37c9c7c3dd6bf7
James Cranwell-Ward <jcranwellward@gmail.com>
2017-08-16 01:29:26 +0300

    disable

--- compose/django/entrypoint.sh
+++ compose/django/entrypoint.sh
@@ -4,35 +4,19 @@ set -e
 touch /home/LogFiles/node_${WEBSITE_ROLE_INSTANCE_ID}_out.log
 echo "$(date) Container started" >> /home/LogFiles/node_${WEBSITE_ROLE_INSTANCE_ID}_out.log
 
-if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
-	# generate fresh rsa key
-	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-fi
-if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
-	# generate fresh dsa key
-	ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
-fi
-
-#prepare run dir
-if [ ! -d "/var/run/sshd" ]; then
-  mkdir -p /var/run/sshd
-fi
-
-/usr/sbin/sshd -D
-
-until psql $DATABASE_URL -c '\l'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "Postgres is up - continuing"
-
-if [ "x$DJANGO_MIGRATE" = 'xon' ]; then
-    /venv/bin/python manage.py migrate --noinput
-fi
-
-if [ "x$DJANGO_COLLECTSTATIC" = 'xon' ]; then
-    /venv/bin/python manage.py collectstatic --noinput
-fi
+#until psql $DATABASE_URL -c '\l'; do
+#  >&2 echo "Postgres is unavailable - sleeping"
+#  sleep 1
+#done
+#
+#>&2 echo "Postgres is up - continuing"
+#
+#if [ "x$DJANGO_MIGRATE" = 'xon' ]; then
+#    /venv/bin/python manage.py migrate --noinput
+#fi
+#
+#if [ "x$DJANGO_COLLECTSTATIC" = 'xon' ]; then
+#    /venv/bin/python manage.py collectstatic --noinput
+#fi
 
 exec "$@"
