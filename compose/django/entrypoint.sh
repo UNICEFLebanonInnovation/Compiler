003fb7396f6d2db1bba945317fd72ff1edeaf00e
James Cranwell-Ward <jcranwellward@gmail.com>
2017-08-15 23:56:06 +0300

    Start ssh

--- compose/django/entrypoint.sh
+++ compose/django/entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 touch /home/LogFiles/node_${WEBSITE_ROLE_INSTANCE_ID}_out.log
 echo "$(date) Container started" >> /home/LogFiles/node_${WEBSITE_ROLE_INSTANCE_ID}_out.log
 
+/etc/init.d/sshd start
+
 until psql $DATABASE_URL -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
