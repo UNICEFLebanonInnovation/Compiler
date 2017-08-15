eaa35c9f680774a209a3b23a0446fada7941e42d
James Cranwell-Ward <jcranwellward@gmail.com>
2017-08-16 00:52:40 +0300

    Prepare ssh

--- compose/django/entrypoint.sh
+++ compose/django/entrypoint.sh
@@ -4,6 +4,20 @@ set -e
 touch /home/LogFiles/node_${WEBSITE_ROLE_INSTANCE_ID}_out.log
 echo "$(date) Container started" >> /home/LogFiles/node_${WEBSITE_ROLE_INSTANCE_ID}_out.log
 
+if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
+	# generate fresh rsa key
+	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+fi
+if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
+	# generate fresh dsa key
+	ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+fi
+
+#prepare run dir
+if [ ! -d "/var/run/sshd" ]; then
+  mkdir -p /var/run/sshd
+fi
+
 /usr/sbin/sshd -D
 
 until psql $DATABASE_URL -c '\l'; do
