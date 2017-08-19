56591c26c6eb969104af7e254acdd26ae7769900
achamseddine <achamseddine@unicef.org>
2017-08-19 23:51:53 +0300

    Add filters, form and tables

---
+++ student_registration/clm/filters.py
@@ -0,0 +1,39 @@
+from django_filters import FilterSet
+
+from .models import BLN, RS, CBECE
+
+
+class BLNFilter(FilterSet):
+    class Meta:
+        model = BLN
+        fields = {
+            'student__first_name': ['contains'],
+            'student__father_name': ['contains'],
+            'student__last_name': ['contains'],
+            'student__mother_fullname': ['contains'],
+            'student__id_number': ['exact'],
+        }
+
+
+class RSFilter(FilterSet):
+    class Meta:
+        model = RS
+        fields = {
+            'student__first_name': ['contains'],
+            'student__father_name': ['contains'],
+            'student__last_name': ['contains'],
+            'student__mother_fullname': ['contains'],
+            'student__id_number': ['exact'],
+        }
+
+
+class CBECEFilter(FilterSet):
+    class Meta:
+        model = CBECE
+        fields = {
+            'student__first_name': ['contains'],
+            'student__father_name': ['contains'],
+            'student__last_name': ['contains'],
+            'student__mother_fullname': ['contains'],
+            'student__id_number': ['exact'],
+        }
