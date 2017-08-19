36fc8dc88b4b09570e75621d1474e499f5e4f99c
achamseddine <achamseddine@unicef.org>
2017-08-19 23:51:53 +0300

    Add filters, form and tables

---
+++ student_registration/clm/serializers.py
@@ -0,0 +1,155 @@
+
+from rest_framework import serializers
+from .models import CLM, BLN, RS, CBECE
+
+
+class CLMSerializer(serializers.ModelSerializer):
+
+    original_id = serializers.IntegerField(source='id', read_only=True)
+    student_id = serializers.IntegerField(source='student.id', required=False)
+    student_first_name = serializers.CharField(source='student.first_name')
+    student_father_name = serializers.CharField(source='student.father_name')
+    student_last_name = serializers.CharField(source='student.last_name')
+    student_full_name = serializers.CharField(source='student.full_name', read_only=True)
+    student_mother_fullname = serializers.CharField(source='student.mother_fullname')
+    student_sex = serializers.CharField(source='student.sex')
+    student_birthday_year = serializers.CharField(source='student.birthday_year')
+    student_birthday_month = serializers.CharField(source='student.birthday_month')
+    student_birthday_day = serializers.CharField(source='student.birthday_day')
+    student_age = serializers.CharField(source='student.calc_age', read_only=True)
+    student_phone = serializers.CharField(source='student.phone')
+    student_phone_prefix = serializers.CharField(source='student.phone_prefix')
+    student_id_number = serializers.CharField(source='student.id_number')
+    student_registered_in_unhcr = serializers.CharField(source='student.registered_in_unhcr')
+    student_id_type = serializers.CharField(source='student.id_type.id')
+    student_id_type_name = serializers.CharField(source='student.id_type.name', read_only=True)
+    student_number = serializers.CharField(source='student.number', read_only=True)
+    student_nationality = serializers.CharField(source='student.nationality')
+    student_mother_nationality = serializers.CharField(source='student.mother_nationality')
+    student_address = serializers.CharField(source='student.address')
+    school_name = serializers.CharField(source='school.name', read_only=True)
+    education_year_name = serializers.CharField(source='education_year.name', read_only=True)
+    school_number = serializers.CharField(source='school.number', read_only=True)
+    section_name = serializers.CharField(source='section.name', read_only=True)
+    classroom_name = serializers.CharField(source='classroom.name', read_only=True)
+    governorate_name = serializers.CharField(source='school.location.parent.name', read_only=True)
+    location = serializers.CharField(source='school.location.name', read_only=True)
+    student_nationality_id = serializers.CharField(source='student.nationality.id', read_only=True)
+    student_mother_nationality_id = serializers.CharField(source='student.mother_nationality.id', read_only=True)
+    student_id_type_id = serializers.CharField(source='student.id_type.id', read_only=True)
+
+    student_outreach_child = serializers.IntegerField(source='student.outreach_child', required=False)
+    student_outreach_child_id = serializers.IntegerField(source='student.outreach_child.id', read_only=True)
+
+    csrfmiddlewaretoken = serializers.IntegerField(source='owner.id', read_only=True)
+    save = serializers.IntegerField(source='owner.id', read_only=True)
+    enrollment_id = serializers.IntegerField(source='id', read_only=True)
+    search_student = serializers.CharField(source='student.full_name', read_only=True)
+    search_school = serializers.CharField(source='school.name', read_only=True)
+    school_type = serializers.CharField(source='school.name', read_only=True)
+    search_barcode = serializers.CharField(source='outreach_barcode', read_only=True)
+
+    def create(self, validated_data):
+        from student_registration.students.serializers import StudentSerializer
+        from student_registration.students.models import Student
+
+        student_data = validated_data.pop('student', None)
+
+        if 'id' in student_data and student_data['id']:
+            student_serializer = StudentSerializer(Student.objects.get(id=student_data['id']), data=student_data)
+            student_serializer.is_valid(raise_exception=True)
+            student_serializer.instance = student_serializer.save()
+        else:
+            student_serializer = StudentSerializer(data=student_data)
+            student_serializer.is_valid(raise_exception=True)
+            student_serializer.instance = student_serializer.save()
+
+        try:
+            instance = Enrollment.objects.create(**validated_data)
+            instance.student = student_serializer.instance
+            instance.save()
+
+        except Exception as ex:
+            raise serializers.ValidationError({'Enrollment instance': ex.message})
+
+        return instance
+
+    def update(self, instance, validated_data):
+
+        student_data = validated_data.pop('student', None)
+
+        if student_data:
+            from student_registration.students.serializers import StudentSerializer
+
+            student_serializer = StudentSerializer(instance.student, data=student_data)
+            student_serializer.is_valid(raise_exception=True)
+            student_serializer.instance = student_serializer.save()
+
+        try:
+
+            for key in validated_data:
+                if hasattr(instance, key):
+                    instance.setattr(key, validated_data[key])
+
+            instance.save()
+
+        except Exception as ex:
+            raise serializers.ValidationError({'Enrollment instance': ex.message})
+
+        return instance
+
+    class Meta:
+        model = CLM
+        fields = (
+            'id',
+            'original_id',
+            'enrollment_id',
+            'student_id',
+            'student_outreach_child',
+            'student_outreach_child_id',
+            'student_first_name',
+            'student_father_name',
+            'student_last_name',
+            'student_full_name',
+            'student_mother_fullname',
+            'student_sex',
+            'student_birthday_year',
+            'student_birthday_month',
+            'student_birthday_day',
+            'student_age',
+            'student_phone',
+            'student_phone_prefix',
+            'student_id_number',
+            'student_id_type',
+            'student_id_type_name',
+            'student_number',
+            'student_nationality',
+            'student_mother_nationality',
+            'student_registered_in_unhcr',
+            'student_address',
+            'school',
+            'school_name',
+            'school_number',
+            'owner',
+            'governorate_name',
+            'location',
+            'student_nationality_id',
+            'student_mother_nationality_id',
+            'student_id_type_id',
+            'last_education_level_id',
+            'last_informal_edu_level_id',
+            'last_informal_edu_round_id',
+            'last_informal_edu_final_result_id',
+            'outreach_barcode',
+            'student_outreached',
+            'registration_date',
+            'new_registry',
+            'have_barcode',
+            'search_school',
+            'search_student',
+            'search_barcode',
+            'csrfmiddlewaretoken',
+            'save',
+            'school_type',
+        )
+
