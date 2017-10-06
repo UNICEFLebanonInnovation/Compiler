24ac172f6af5057cc3d3ef23f7fe8911649b4a71
achamseddine <achamseddine@unicef.org>
2017-10-06 13:03:18 +0300

    Migrate data and cleanup
    Migrate ‘registered_in_unhcr’ to student instance
    Migrate gradings from enrollments to new model
    Cleanup enrollments duplications

---
+++ student_registration/enrollments/management/commands/migrate_gradings.py
@@ -0,0 +1,12 @@
+__author__ = 'achamseddine'
+
+from django.core.management.base import BaseCommand
+
+from student_registration.enrollments.tasks import *
+
+
+class Command(BaseCommand):
+    help = 'Migrate enrollment gradings to the new structure'
+
+    def handle(self, *args, **options):
+        migrate_gradings()
