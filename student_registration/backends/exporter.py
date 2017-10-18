c293f6ea4d7227156c84e7afc70bff48bb1f5f71
achamseddine <achamseddine@unicef.org>
2017-10-18 09:48:58 +0300

    Add default settings, centralize export process

--- student_registration/backends/exporter.py
+++ student_registration/backends/exporter.py
@@ -2,7 +2,12 @@
 
 def export_full_data(params):
     import tasks
+    import time
+    from .file import store_file
+
+    timestamp = time.time()
     report = params['report']
     method_to_call = getattr(tasks, report)
-    method_to_call.delay(params)
+    data = method_to_call.delay(params)
+    store_file(data, timestamp)
 
