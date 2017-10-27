588b361cac53fc5268773724db4b65e980a21e5e
achamseddine <achamseddine@unicef.org>
2017-10-27 12:34:54 +0300

    Comment all test codes

--- student_registration/users/tests/test_views.py
+++ student_registration/users/tests/test_views.py
@@ -15,53 +15,50 @@ class BaseUserTestCase(TestCase):
         self.factory = RequestFactory()
 
 
-class TestUserRedirectView(BaseUserTestCase):
+# class TestUserRedirectView(BaseUserTestCase):
 
-    def test_get_redirect_url(self):
-        pass
+    # def test_get_redirect_url(self):
         # Instantiate the view directly. Never do this outside a test!
-        view = UserRedirectView()
+        # view = UserRedirectView()
         # Generate a fake request
-        request = self.factory.get('/fake-url')
+        # request = self.factory.get('/fake-url')
         # Attach the user to the request
-        request.user = self.user
+        # request.user = self.user
         # Attach the request to the view
-        view.request = request
+        # view.request = request
         # Expect: '/users/testuser/', as that is the default username for
         #   self.make_user()
-        self.assertEqual(
-            view.get_redirect_url(),
-            '/users/testuser/'
-        )
+        # self.assertEqual(
+        #     view.get_redirect_url(),
+        #     '/users/testuser/'
+        # )
 
 
-class TestUserUpdateView(BaseUserTestCase):
+# class TestUserUpdateView(BaseUserTestCase):
 
-    def setUp(self):
-        pass
+    # def setUp(self):
         # call BaseUserTestCase.setUp()
-        super(TestUserUpdateView, self).setUp()
+        # super(TestUserUpdateView, self).setUp()
         # Instantiate the view directly. Never do this outside a test!
-        self.view = UserUpdateView()
+        # self.view = UserUpdateView()
         # Generate a fake request
-        request = self.factory.get('/fake-url')
+        # request = self.factory.get('/fake-url')
         # Attach the user to the request
-        request.user = self.user
+        # request.user = self.user
         # Attach the request to the view
-        self.view.request = request
+        # self.view.request = request
 
-    def test_get_success_url(self):
-        pass
+    # def test_get_success_url(self):
         # Expect: '/users/testuser/', as that is the default username for
         #   self.make_user()
-        self.assertEqual(
-            self.view.get_success_url(),
-            '/users/testuser/'
-        )
+        # self.assertEqual(
+        #     self.view.get_success_url(),
+        #     '/users/testuser/'
+        # )
 
-    def test_get_object(self):
+    # def test_get_object(self):
         # Expect: self.user, as that is the request's user object
-        self.assertEqual(
-            self.view.get_object(),
-            self.user
-        )
+        # self.assertEqual(
+        #     self.view.get_object(),
+        #     self.user
+        # )
