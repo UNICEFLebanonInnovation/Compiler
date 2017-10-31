36a1a7f2371707926926995c18760e5c2aa0cf1b
achamseddine <achamseddine@unicef.org>
2017-10-31 16:56:28 +0200

    Rollback Beneficiary model to the base version

--- student_registration/winterization/admin.py
+++ student_registration/winterization/admin.py
@@ -6,34 +6,34 @@ from import_export.admin import ImportExportModelAdmin
 from import_export.widgets import *
 from .models import Beneficiary
 
-
-class BeneficiaryResource(resources.ModelResource):
-
-    class Meta:
-        model = Beneficiary
-        fields = (
-
-        )
-        export_order = fields
-
-
-class BeneficiaryAdmin(ImportExportModelAdmin):
-    resource_class = BeneficiaryResource
-    list_display = (
-        '_id',
-    )
-    list_filter = (
-        'assistance_type',
-    )
-    search_fields = (
-        'id_type',
-    )
-
-    def get_queryset(self, request):
-        if request.user.id == 1:
-            return super(BeneficiaryAdmin, self).get_queryset(request)
-        return Beneficiary.objects.none()
-
-
-admin.site.register(Beneficiary, BeneficiaryAdmin)
+#
+# class BeneficiaryResource(resources.ModelResource):
+#
+#     class Meta:
+#         model = Beneficiary
+#         fields = (
+#
+#         )
+#         export_order = fields
+#
+#
+# class BeneficiaryAdmin(ImportExportModelAdmin):
+#     resource_class = BeneficiaryResource
+#     list_display = (
+#         '_id',
+#     )
+#     list_filter = (
+#         'assistance_type',
+#     )
+#     search_fields = (
+#         'id_type',
+#     )
+#
+#     def get_queryset(self, request):
+#         if request.user.id == 1:
+#             return super(BeneficiaryAdmin, self).get_queryset(request)
+#         return Beneficiary.objects.none()
+#
+#
+# admin.site.register(Beneficiary, BeneficiaryAdmin)
 
