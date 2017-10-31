e68b83fab32fde2b138f1ba64ebd4bc443c5055b
achamseddine <achamseddine@unicef.org>
2017-10-31 17:40:12 +0200

    Create Assessment model for winter

--- student_registration/winterization/admin.py
+++ student_registration/winterization/admin.py
@@ -4,36 +4,36 @@ from import_export import resources, fields
 from import_export import fields
 from import_export.admin import ImportExportModelAdmin
 from import_export.widgets import *
-from .models import Beneficiary
+from .models import Assessment
 
-#
-# class BeneficiaryResource(resources.ModelResource):
-#
-#     class Meta:
-#         model = Beneficiary
-#         fields = (
-#
-#         )
-#         export_order = fields
-#
-#
-# class BeneficiaryAdmin(ImportExportModelAdmin):
-#     resource_class = BeneficiaryResource
-#     list_display = (
-#         '_id',
-#     )
-#     list_filter = (
-#         'assistance_type',
-#     )
-#     search_fields = (
-#         'id_type',
-#     )
-#
-#     def get_queryset(self, request):
-#         if request.user.id == 1:
-#             return super(BeneficiaryAdmin, self).get_queryset(request)
-#         return Beneficiary.objects.none()
-#
-#
-# admin.site.register(Beneficiary, BeneficiaryAdmin)
-#
+
+class AssessmentResource(resources.ModelResource):
+
+    class Meta:
+        model = Assessment
+        fields = (
+
+        )
+        export_order = fields
+
+
+class AssessmentAdmin(ImportExportModelAdmin):
+    resource_class = AssessmentResource
+    list_display = (
+        '_id',
+    )
+    list_filter = (
+        'assistance_type',
+    )
+    search_fields = (
+        'id_type',
+    )
+
+    def get_queryset(self, request):
+        if request.user.id == 1:
+            return super(AssessmentAdmin, self).get_queryset(request)
+        return Assessment.objects.none()
+
+
+admin.site.register(Assessment, AssessmentAdmin)
+
