c020fecfa313ef8e7ef555da26f871106a7e1191
achamseddine <achamseddine@unicef.org>
2017-11-06 14:30:02 +0200

    Import outreach data script

--- student_registration/outreach/management/commands/read_file.py
+++ student_registration/outreach/management/commands/read_file.py
@@ -2,18 +2,20 @@ __author__ = 'achamseddine'
 
 from django.core.management.base import BaseCommand
 
-# from student_registration.outreach.tasks import read_file
+from student_registration.outreach.tasks import read_file
 
 
 class Command(BaseCommand):
     help = 'Read outreach file'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--url', type=str, default=None)
+        parser.add_argument('--token', type=str, default=None)
+        parser.add_argument('--protocol', type=str, default='HTTPS')
+
     def handle(self, *args, **options):
-        from openpyxl import load_workbook
-
-        wb = load_workbook(filename='outreach_data.xlsx', read_only=True)
-        ws = wb['big_data']
-
-        for row in ws.rows:
-            for cell in row:
-                print(cell.value)
+        read_file(
+            base_url=options['url'],
+            token=options['token'],
+            protocol=options['protocol']
+        )
