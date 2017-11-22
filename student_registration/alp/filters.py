2ce31858d59bdb3967fa048986f98c1696bf7b19
achamseddine <achamseddine@unicef.org>
2017-11-22 20:33:36 +0200

    Refining compiler (#382)
    * Add save and add another button
    
    * Exclude close reason from the queryset
    
    * Remove cycle from BLN
    
    * Improvement per domain
    
    * Pre and post test grading ALP
    
    * Add age limite
    
    * Update old data
    
    * CLM feedback
    
    * Customize list filters and add empty label
    
    * Load nationality for old students

--- student_registration/alp/filters.py
+++ student_registration/alp/filters.py
@@ -1,6 +1,10 @@
-from django_filters import FilterSet
+from django.utils.translation import ugettext as _
 
-from .models import Outreach
+from django_filters import FilterSet, ModelChoiceFilter
+
+from student_registration.schools.models import School, Section, EducationLevel
+from student_registration.students.models import Nationality
+from .models import Outreach, ALPRound
 
 
 class OutreachFilter(FilterSet):
@@ -16,6 +20,9 @@ class OutreachFilter(FilterSet):
 
 
 class PreTestFilter(FilterSet):
+    level = ModelChoiceFilter(queryset=EducationLevel.objects.all(), empty_label=_('Entrance test'))
+    school = ModelChoiceFilter(queryset=School.objects.all(), empty_label=_('School'))
+
     class Meta:
         model = Outreach
         fields = {
@@ -29,12 +36,17 @@ class PreTestFilter(FilterSet):
 
 
 class PostTestFilter(FilterSet):
+    registered_in_level = ModelChoiceFilter(queryset=EducationLevel.objects.all(), empty_label=_('Current Level'))
+    school = ModelChoiceFilter(queryset=School.objects.all(), empty_label=_('School'))
+    section = ModelChoiceFilter(queryset=Section.objects.all(), empty_label=_('Section'))
+
     class Meta:
         model = Outreach
         fields = {
             'school': ['exact'],
             'post_test_room': ['exact'],
             'registered_in_level': ['exact'],
+            'section': ['exact'],
             'student__first_name': ['contains'],
             'student__father_name': ['contains'],
             'student__last_name': ['contains'],
@@ -42,11 +54,16 @@ class PostTestFilter(FilterSet):
 
 
 class SchoolFilter(FilterSet):
+    registered_in_level = ModelChoiceFilter(queryset=EducationLevel.objects.all(), empty_label=_('Current Level'))
+    section = ModelChoiceFilter(queryset=Section.objects.all(), empty_label=_('Section'))
+    student__nationality = ModelChoiceFilter(queryset=Nationality.objects.all(), empty_label=_('Nationality'))
+
     class Meta:
         model = Outreach
         fields = {
             'registered_in_level': ['exact'],
             'section': ['exact'],
+            'student__nationality': ['exact'],
             'student__first_name': ['contains'],
             'student__father_name': ['contains'],
             'student__last_name': ['contains'],
