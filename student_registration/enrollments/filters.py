c78525fc8dee10854d9676351e09baea01523c38
achamseddine <achamseddine@unicef.org>
2017-11-22 13:29:22 +0200

    Customize list filters and add empty label

--- student_registration/enrollments/filters.py
+++ student_registration/enrollments/filters.py
@@ -1,25 +1,38 @@
-from django_filters import FilterSet
+from django.utils.translation import ugettext as _
 
+from django_filters import FilterSet, ModelChoiceFilter
+
+from student_registration.locations.models import Location
+from student_registration.schools.models import EducationYear, Section, ClassRoom
+from student_registration.students.models import Nationality
 from .models import Enrollment
 
 
-class EnrollmentFilter(FilterSet):
+class CommonFilter(FilterSet):
+    student__nationality = ModelChoiceFilter(queryset=Nationality.objects.all(), empty_label=_('Nationality'))
+    section = ModelChoiceFilter(queryset=Section.objects.all(), empty_label=_('Section'))
+    classroom = ModelChoiceFilter(queryset=ClassRoom.objects.all(), empty_label=_('Class'))
+
+
+class EnrollmentFilter(CommonFilter):
     class Meta:
         model = Enrollment
         fields = {
-            # 'education_year': ['exact'],
             'classroom': ['exact'],
             'section': ['exact'],
-            # 'exam_result': ['exact'],
             'student__first_name': ['contains'],
             'student__father_name': ['contains'],
             'student__last_name': ['contains'],
             'student__mother_fullname': ['contains'],
-            'student__id_number': ['exact'],
+            'student__id_number': ['contains'],
+            'student__nationality': ['exact'],
         }
 
 
-class EnrollmentOldDataFilter(FilterSet):
+class EnrollmentOldDataFilter(CommonFilter):
+    education_year = ModelChoiceFilter(queryset=EducationYear.objects.filter(current_year=False),
+                                       empty_label=_('Education year'))
+
     class Meta:
         model = Enrollment
         fields = {
@@ -30,5 +43,6 @@ class EnrollmentOldDataFilter(FilterSet):
             'student__father_name': ['contains'],
             'student__last_name': ['contains'],
             'student__mother_fullname': ['contains'],
-            'student__id_number': ['exact'],
+            'student__id_number': ['contains'],
+            'student__nationality': ['exact'],
         }
