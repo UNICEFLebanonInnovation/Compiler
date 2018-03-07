261574d5f334ea55afe49527d91136373157f20e
achamseddine <achamseddine@unicef.org>
2018-03-07 14:19:12 +0200

    Change max length grading

--- student_registration/enrollments/models.py
+++ student_registration/enrollments/models.py
@@ -629,84 +629,84 @@ class EnrollmentGrading(models.Model):
     )
 
     exam_result_arabic = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         verbose_name=_('Arabic')
     )
 
     exam_result_language = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         verbose_name=_('Foreign language')
     )
 
     exam_result_education = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         verbose_name=_('Education')
     )
 
     exam_result_geo = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         verbose_name=_('Geography')
     )
 
     exam_result_history = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         verbose_name=_('History')
     )
 
     exam_result_math = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         verbose_name=_('Math')
     )
 
     exam_result_science = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         verbose_name=_('Science')
     )
 
     exam_result_physic = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         verbose_name=_('Physic')
     )
 
     exam_result_chemistry = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         verbose_name=_('Chemistry')
     )
 
     exam_result_bio = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         verbose_name=_('Biology')
     )
 
     exam_result_linguistic_ar = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         default=None,
         verbose_name=_('Linguistic field/Arabic')
     )
     exam_result_linguistic_en = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         default=None,
@@ -714,7 +714,7 @@ class EnrollmentGrading(models.Model):
     )
 
     exam_result_sociology = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         default=None,
@@ -722,7 +722,7 @@ class EnrollmentGrading(models.Model):
     )
 
     exam_result_physical = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         default=None,
@@ -730,7 +730,7 @@ class EnrollmentGrading(models.Model):
     )
 
     exam_result_artistic = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         default=None,
@@ -738,7 +738,7 @@ class EnrollmentGrading(models.Model):
     )
 
     exam_result_mathematics = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         default=None,
@@ -746,7 +746,7 @@ class EnrollmentGrading(models.Model):
     )
 
     exam_result_sciences = models.CharField(
-        max_length=4,
+        max_length=6,
         blank=True,
         null=True,
         default=None,
