a2d54d9ebcf8c1e7c953f0109ea57128472b2e28
achamseddine <achamseddine@unicef.org>
2018-11-07 12:30:55 +0200

    Replace gis.db by db

--- student_registration/backends/models.py
+++ student_registration/backends/models.py
@@ -2,7 +2,8 @@ from __future__ import unicode_literals, absolute_import, division
 
 from django.conf import settings
 from django.utils.translation import ugettext as _
-from django.contrib.gis.db import models
+from django.db import models
+# from django.contrib.gis.db import models
 from django.db.models.signals import post_save
 from django.utils.encoding import force_text
 
