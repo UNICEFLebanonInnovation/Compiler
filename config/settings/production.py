4fa53bfcaae4ac8de368fcb7e7b7323e9ded68b0
achamseddine <achamseddine@unicef.org>
2019-02-01 17:43:07 +0200

    few changes 1

--- config/settings/production.py
+++ config/settings/production.py
@@ -213,6 +213,3 @@ ADMIN_URL = env('DJANGO_ADMIN_URL')
 
 # Your production stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
-
-# 6.2.1 Cookie Security: Cookie not Sent Over SSL
-SESSION_COOKIE_SECURE = True
