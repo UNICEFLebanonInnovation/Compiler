77ae4c47b76806a4ec5ac00c472e58b8415e5761
Geovanidib <44754402+Geovanidib@users.noreply.github.com>
2019-07-03 08:31:02 +0300

    attendance_by_student
    generate Daily Attendance by student

--- student_registration/attendances/utils.py
+++ student_registration/attendances/utils.py
@@ -94,25 +94,43 @@ def find_attendances(governorate=None, student_id=None, from_date=None, to_date=
     return data
 
 
-def add_attendance(attendance, students):
+def add_attendance(attendance, students, std_id):
     from .models import AttendanceDt
 
     for student in students:
-        try:
-            AttendanceDt.objects.get(student_id=student['student_id'], attendance_date=attendance.attendance_date)
-        except AttendanceDt.DoesNotExist:
-            attendance_dt = AttendanceDt.objects.create(
-                student_id=student['student_id'],
-                attendance_date=attendance.attendance_date,
-                is_present=student['status'],
-                school_id=attendance.school_id,
-                section_id=student['section'],
-                classlevel_id=student['level'],
-                classroom_id=attendance.classroom_id,
-                attendance_id=attendance.id,
-                levelname=student['level_name'],
-            )
-            attendance_dt.save()
+        if std_id != "":
+            if std_id == student['student_id']:
+                try:
+                    AttendanceDt.objects.get(student_id=std_id, attendance_date=attendance.attendance_date)
+                except AttendanceDt.DoesNotExist:
+                    attendance_dt = AttendanceDt.objects.create(
+                        student_id=student['student_id'],
+                        attendance_date=attendance.attendance_date,
+                        is_present=student['status'],
+                        school_id=attendance.school_id,
+                        section_id=student['section'],
+                        classlevel_id=student['level'],
+                        classroom_id=attendance.classroom_id,
+                        attendance_id=attendance.id,
+                        levelname=student['level_name'],
+                        )
+                    attendance_dt.save()
+        else:
+            try:
+                AttendanceDt.objects.get(student_id=student['student_id'], attendance_date=attendance.attendance_date)
+            except AttendanceDt.DoesNotExist:
+                attendance_dt = AttendanceDt.objects.create(
+                    student_id=student['student_id'],
+                    attendance_date=attendance.attendance_date,
+                    is_present=student['status'],
+                    school_id=attendance.school_id,
+                    section_id=student['section'],
+                    classlevel_id=student['level'],
+                    classroom_id=attendance.classroom_id,
+                    attendance_id=attendance.id,
+                    levelname=student['level_name'],
+                    )
+                attendance_dt.save()
 
 
 def calculate_absentees(attendance, students):
