350eaff68d673ca16d70071f6e0868e124d3efad
achamseddine <achamseddine@unicef.org>
2020-01-06 13:12:31 +0200

    English labeling

--- student_registration/locations/admin.py
+++ student_registration/locations/admin.py
@@ -18,6 +18,7 @@ class LocationResource(resources.ModelResource):
         fields = (
             'id',
             'name',
+            'name_en',
             'type',
             'parent',
             'latitude',
