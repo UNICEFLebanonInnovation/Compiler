52006d93163515224decd17ff97337f7219c5b82
Yosr Beydoun <ybaydoun@hotmail.com>
2023-01-06 16:52:21 +0200

    Child Profile Layout

--- student_registration/mscc/services_form.py
+++ student_registration/mscc/services_form.py
@@ -56,7 +56,7 @@ class PSSServiceForm(forms.ModelForm):
         label=_('Do you feel distressed and anxious?')
     )
     caregivers_additional_parenting = forms.ChoiceField(
-        widget=forms.Select, required=True,
+        widget=forms.Select, required=False,
         choices=YES_NO,
         label=_('If yes, would you like any additional parenting or psychosocial support?')
     )
@@ -67,7 +67,7 @@ class PSSServiceForm(forms.ModelForm):
                        'signs of distress or negative mental health symptoms ?')
     )
     child_additional_parenting = forms.ChoiceField(
-        widget=forms.Select, required=True,
+        widget=forms.Select, required=False,
         choices=YES_NO,
         label=_('If yes, do you need additional support '
                        'for taking care or better dealing with your children?')
@@ -133,8 +133,8 @@ class PSSServiceForm(forms.ModelForm):
         instance.child_vulnerability = validated_data.get('child_vulnerability')
         instance.child_out_school_reasons = validated_data.get('child_out_school_reasons')
         instance.caregivers_distress = validated_data.get('caregivers_distress')
-        instance.child_additional_parenting = validated_data.get('caregivers_additional_parenting')
-        instance.child_additional_parenting = validated_data.get('child_distress')
+        instance.caregivers_additional_parenting = validated_data.get('caregivers_additional_parenting')
+        instance.child_distress = validated_data.get('child_distress')
         instance.child_additional_parenting = validated_data.get('child_additional_parenting')
         instance.modified_by = request.user
         instance.save()
@@ -830,12 +830,12 @@ class FollowUpServiceForm(forms.ModelForm):
         validated_data = request.POST
 
         if not instance:
-            instance = YouthKitService.objects.create(registration_id=registry)
+            instance = FollowUpService.objects.create(registration_id=registry)
         else:
-            instance = YouthKitService.objects.get(id=instance)
+            instance = FollowUpService.objects.get(id=instance)
 
         instance.follow_up_type = validated_data.get('follow_up_type')
-        instance.phone_call_number = validated_data.get('phone_call_number,, 	')
+        instance.phone_call_number = validated_data.get('phone_call_number')
         instance.house_visit_number = validated_data.get('house_visit_number')
         instance.caregiver_visit_number = validated_data.get('caregiver_visit_number')
         instance.follow_up_result = validated_data.get('follow_up_result')
