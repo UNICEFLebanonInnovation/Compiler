0e491aaba1a47b7b1e4f2b8a22c4b2a85f37f2a1
Yosr Beydoun <ybaydoun@hotmail.com>
2023-05-10 14:36:57 +0300

    export missing file

---
+++ student_registration/students/export.py
@@ -0,0 +1,33 @@
+from import_export import resources, fields
+from .models import Teacher
+
+
+
+class TeacherResource(resources.ModelResource):
+    class Meta:
+        model = Teacher
+        fields = (
+            'id',
+            'first_name',
+            'father_name',
+            'last_name',
+            'sex',
+            'primary_phone_number',
+            'school',
+            'email',
+            'subjects_provided',
+            'registration_level',
+            'teacher_assignment',
+            'teaching_hours_private_school',
+            'teaching_hours_dirasa',
+            'trainings',
+            'training_sessions_attended',
+            'extra_coaching',
+            'extra_coaching_specify',
+            'owner__username',
+            'modified_by__username',
+            'created',
+            'modified',
+        )
+        export_order = fields
+
