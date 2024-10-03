f656363dbfc400c4eb58abdba9dd64e4741b5877
Yosr Beydoun <ybaydoun@hotmail.com>
2024-10-03 11:48:37 +0300

    school Health visit and meeting dtp

---
+++ student_registration/static/js/school/meeting.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+
+     if($(document).find('#id_meeting_date').length == 1) {
+        $('#id_meeting_date').datepicker({dateFormat: "yy-mm-dd"});
+    }
+
+});
+
+
+
