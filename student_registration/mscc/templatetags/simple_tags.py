6f047f91462a29a058da1e75f8a87a4a67294271
Yosr Beydoun <ybaydoun@hotmail.com>
2024-11-12 20:38:52 +0300

    Education Situation History

--- student_registration/mscc/templatetags/simple_tags.py
+++ student_registration/mscc/templatetags/simple_tags.py
@@ -77,6 +77,14 @@ def get_service_all(registry, model_name):
 
 
 @register.simple_tag
+def get_education_service_history(child_id):
+    try:
+        return EducationService.objects.filter(registration__child_id=child_id, registration__deleted=False)
+    except Exception as ex:
+        return False
+
+
+@register.simple_tag
 def get_services(registry):
     return ProvidedServices.objects.filter(registration=registry)
 
