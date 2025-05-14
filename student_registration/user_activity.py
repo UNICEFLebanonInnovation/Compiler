2b3fd0bed69b24f6d2f000e0e557979ee23f9994
Ali Chamseddine <achamseddine@unicef.org>
2025-05-14 13:47:55 +0300

    User activity

---
+++ student_registration/user_activity.py
@@ -0,0 +1,37 @@
+# middleware/user_activity.py
+import json
+from django.utils.deprecation import MiddlewareMixin
+
+from student_registration.backends.models import UserActivity
+
+
+class UserActivityMiddleware(MiddlewareMixin):
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        try:
+            if request.user.is_authenticated():
+                if request.method == "POST":
+                    data = request.POST.copy()
+                else:
+                    data = request.GET.copy()
+
+                if request.path.startswith('/admin'):
+                   return response
+
+                # Serialize to JSON string (handle complex data manually if needed)
+                serialized_data = json.dumps(dict(data.lists()))
+
+                UserActivity.objects.create(
+                    username=request.user,
+                    path=request.path,
+                    method=request.method,
+                    data=serialized_data
+                )
+        except Exception as e:
+            print(e)
+            pass  # Optional: log the error to avoid breaking the app
+
+        return response
