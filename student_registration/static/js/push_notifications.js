696d7fca9749f93920d4e31c01e78de959fd142a
Ali Chamseddine <achamseddine@unicef.org>
2025-07-07 11:04:46 +0300

    Append export notifications in header

--- student_registration/static/js/push_notifications.js
+++ student_registration/static/js/push_notifications.js
@@ -24,11 +24,22 @@
 
     messaging.onMessage(function(payload){
         if(payload.data && payload.data.file){
-            $('#downloadLink').attr('href', '/MSCC/export-download/' + payload.data.file + '/');
+            var file = payload.data.file;
+            var link = '/MSCC/export-download/' + file + '/';
+            $('#downloadLink').attr('href', link);
+            var message = 'Your export file is ready.';
             if(payload.notification && payload.notification.body){
-                $('#downloadModalMessage').text(payload.notification.body);
+                message = payload.notification.body;
+                $('#downloadModalMessage').text(message);
             }
             $('#downloadModal').modal('show');
+
+            var itemHtml = '<div class="vertical-timeline-item dot-success vertical-timeline-element">' +
+                '<div><span class="vertical-timeline-element-icon bounce-in"></span>' +
+                '<div class="vertical-timeline-element-content bounce-in">' +
+                '<h4 class="timeline-title"><a href="' + link + '" target="_blank">' + message + '</a></h4>' +
+                '</div></div></div>';
+            $('#notificationList').prepend(itemHtml);
         }
     });
 })();
