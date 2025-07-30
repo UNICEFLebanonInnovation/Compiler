b104feb0df024734cb2f7055642c19b7da1a85d6
Ali Chamseddine <achamseddine@unicef.org>
2025-07-30 13:10:15 +0300

    Move pivot dashboard to dashboard app

--- student_registration/mscc/views.py
+++ student_registration/mscc/views.py
@@ -137,6 +137,8 @@ class DashboardView(LoginRequiredMixin,
         return {}
 
 
+
+
 class DashboardCustomView(LoginRequiredMixin,
                     TemplateView):
     template_name = 'mscc/dashboard.html'
