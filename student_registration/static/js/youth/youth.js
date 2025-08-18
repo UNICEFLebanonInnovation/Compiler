904afc8670c052727bbd96323123dfc9a6587069
Ali Chamseddine <achamseddine@unicef.org>
2025-08-18 12:25:02 +0300

    Check required fields before advancing in youth form

--- student_registration/static/js/youth/youth.js
+++ student_registration/static/js/youth/youth.js
@@ -180,27 +180,6 @@ $(document).ready(function() {
         checkEnglishOnly($(this));
     });
 
-    $(document).on('click', '#next-page', function(e){
-        e.preventDefault();
-        $(this).removeClass('error-field');
-        var error_fields = false;
-        $('input, select').filter('[required]:visible').each(function(){
-            if($(this).val() == null || $(this).val() == ''){
-                $(this).addClass('error-field');
-                error_fields = true;
-            }
-        });
-        if(typeof validateMainForm === 'function' && !validateMainForm(false)){
-            error_fields = true;
-        }
-        if(!error_fields){
-            $('#next-btn22').trigger('click');
-            $(this).removeClass('error-field');
-         }else{
-            $('#formErrorModal').modal('show');
-         }
-    });
-
 
 });
 
