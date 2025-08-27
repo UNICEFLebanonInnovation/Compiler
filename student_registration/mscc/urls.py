1fcc583adb0b3a89edecbb07748e15e5c0642d10
Ali Chamseddine <achamseddine@unicef.org>
2025-08-27 15:49:14 +0300

    Create heatmap template and script for attendance

--- student_registration/mscc/urls.py
+++ student_registration/mscc/urls.py
@@ -289,6 +289,11 @@ urlpatterns = [
         name='attendance_report'
     ),
     re_path(
+        'attendance-heatmap/$',
+        attendance_views.AttendanceHeatmap.as_view(),
+        name='attendance_heatmap'
+    ),
+    re_path(
         r'^services/follow-up-view-all/(?P<registry>[\w.@+-]+)/$',
         view=services_view.FollowUpViewAll.as_view(),
         name='service_follow_up_view_all'
