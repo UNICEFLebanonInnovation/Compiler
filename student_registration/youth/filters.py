01261a28395c2a45193a17e2ea373aed9e51c11b
Yosr Beydoun <ybaydoun@hotmail.com>
2025-09-12 19:04:22 +0300

    Youth Filters

--- student_registration/youth/filters.py
+++ student_registration/youth/filters.py
@@ -10,7 +10,8 @@ from django_filters import (
     ModelMultipleChoiceFilter,
     MultipleChoiceFilter
 )
-from django.db.models import Q
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, ButtonHolder, Submit, HTML
 import datetime
 
 
@@ -33,7 +34,38 @@ from student_registration.clm.models import Disability, EducationalLevel
 from student_registration.schools.models import PartnerOrganization
 
 
-class MainFilter(FilterSet):
+class PlaceholderFilterSet(FilterSet):
+    """Base FilterSet that hides labels and uses placeholders."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.form.helper = FormHelper(self.form)
+        self.form.helper.form_method = "get"     # django-filter expects GET
+        self.form.helper.form_class = "form-inline"
+        self.form.helper.form_tag = True
+        # self.form.helper.add_input(Submit("submit", "Filter"))
+        # self.form.helper.add_input(Rest("Rest", "Cancel"))
+        all_fields = list(self.form.fields)  # -> ['type', 'partner', 'round', ...]
+        self.form.helper.layout = Layout(
+            *all_fields,
+            ButtonHolder(
+                Submit("submit", "Filter", css_class="btn btn-primary"),
+                HTML(
+                    '<a href="#" title="Download" class="btn btn-outline-success download-report">'
+                    '<i class="lnr-download"></i>'
+                    '</a>'
+                ),
+                css_class="d-flex gap-2"  # optional: layout/spacing
+            ),
+        )
+        for name, field in self.form.fields.items():
+            label = field.label or name.replace('_', ' ').title()
+            field.label = ''
+            if isinstance(field.widget, (forms.TextInput, forms.NumberInput)):
+                field.widget.attrs.setdefault('placeholder', label)
+
+
+class MainFilter(PlaceholderFilterSet):
     adolescent__nationality = ChoiceFilter(choices=Nationality.objects.values_list('id', 'name')
                                 .order_by('name').distinct(), empty_label='Nationality')
 
@@ -50,7 +82,7 @@ class MainFilter(FilterSet):
         ]
 
 
-class FullFilter(FilterSet):
+class FullFilter(PlaceholderFilterSet):
     partner = ChoiceFilter(
         choices=PartnerOrganization.objects.filter(active=True, is_youth=True).values_list('id', 'short_name').order_by('short_name').distinct(), empty_label='Partner')
     adolescent__governorate = ChoiceFilter(
@@ -153,7 +185,8 @@ class FullFilter(FilterSet):
     def filter_by_program_document(self, queryset, name, value):
         return queryset.filter(enrolled_programs__program_document=value)
 
-class PartnerFilter(FilterSet):
+
+class PartnerFilter(PlaceholderFilterSet):
 
     adolescent__governorate = ChoiceFilter(
         choices=Location.objects.filter(parent__isnull=True).values_list('id', 'name').order_by('name').distinct(),
@@ -256,7 +289,7 @@ class PartnerFilter(FilterSet):
         return queryset.filter(enrolled_programs__program_document=value)
 
 
-class PDFilter(FilterSet):
+class PDFilter(PlaceholderFilterSet):
     current_year = datetime.datetime.now().year
     partner = ChoiceFilter(choices=PartnerOrganization.objects.filter(active=True, is_youth=True).values_list('id', 'short_name')
                                 .order_by('short_name').distinct(), empty_label='Partner')
@@ -307,7 +340,7 @@ class PDFilter(FilterSet):
         return queryset
 
 
-class PDPartnerFilter(FilterSet):
+class PDPartnerFilter(PlaceholderFilterSet):
     current_year = datetime.datetime.now().year
     funded_by = ChoiceFilter(choices=FundedBy.objects.filter(active=True).values_list('id', 'name')
                                  .order_by('name').distinct(), empty_label='Funded By')
