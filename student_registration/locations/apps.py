eb5ac4df66920db7a30e99e3ad1730b86db2412a
Ali Chamseddine <achamseddine@unicef.org>
2025-09-17 12:02:33 +0300

    Remove django.db.models.BigAutoField from locations app

--- student_registration/locations/apps.py
+++ student_registration/locations/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 
 class LocationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    # default_auto_field = 'django.db.models.BigAutoField'
     name = 'student_registration.locations'
