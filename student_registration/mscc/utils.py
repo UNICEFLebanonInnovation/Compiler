33869dae04aa755a322e3366003030f335aa9454
Yosr Beydoun <ybaydoun@hotmail.com>
2025-09-22 12:19:21 +0300

    Revert "Merge pull request #945 from UNICEFLebanonInnovation/codex/optimize-n+1-query-in-child-profile"
    This reverts commit 12062aa5840bbf48d291eab3cd237b2d069a47d0, reversing
    changes made to 041fe58e870daf4810ee9b7db5bc11bf6b80effc.

--- student_registration/mscc/utils.py
+++ student_registration/mscc/utils.py
@@ -42,40 +42,16 @@ def generate_services(child_age, registry, user=None):
         from .models import ProvidedServices, Packages
         from student_registration.users.templatetags.custom_tags import has_group
 
-        packages_qs = Packages.objects.filter(type=registry.type, age=child_age)
+        packages = Packages.objects.filter(type=registry.type, age=child_age)
         if user and has_group(user, 'MSCC_YOUTH'):
-            packages_qs = packages_qs.filter(category="Youth")
+            packages = packages.filter(category="Youth")
 
-        packages = list(packages_qs)
-        if not packages:
-            return True
-
-        existing_services = set(
-            ProvidedServices.objects.filter(
-                registration=registry,
-                name__in=[package.name for package in packages],
-            ).values_list("name", "type", "category")
-        )
-
-        services_to_create = []
-        for package in packages:
-            key = (package.name, package.type, package.category)
-            if key in existing_services:
-                continue
-
-            services_to_create.append(
-                ProvidedServices(
-                    name=package.name,
-                    registration=registry,
-                    type=package.type,
-                    category=package.category,
-                )
-            )
-            existing_services.add(key)
-
-        if services_to_create:
-            ProvidedServices.objects.bulk_create(services_to_create)
-        return True
+        for package in packages.all():
+            instance, created = ProvidedServices.objects.get_or_create(name=package.name,
+                                                                       registration=registry,
+                                                                       type=package.type,
+                                                                       category=package.category)
+            instance.save()
     except Exception as ex:
         return False
 
