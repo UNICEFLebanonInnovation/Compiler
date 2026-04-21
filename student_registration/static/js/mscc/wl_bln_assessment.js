b7914b18c0e086024bd9ad268ebf62e721b8e2fb
Yosr Beydoun <ybaydoun@hotmail.com>
2026-04-21 18:38:50 +0300

    Update WL BLN totals immediately when a score field changes

--- student_registration/static/js/mscc/wl_bln_assessment.js
+++ student_registration/static/js/mscc/wl_bln_assessment.js
@@ -1,32 +1,33 @@
 $(document).ready(function () {
-    function updateTotals() {
-        var totals = {};
+    function calculateTotalForField(totalField) {
+        var total = 0;
 
-        $('[data-wl-bln-component="1"]').each(function () {
-            var totalField = $(this).data('wl-bln-total-target');
+        $('[data-wl-bln-component="1"][data-wl-bln-total-target="' + totalField + '"]').each(function () {
             var value = parseFloat($(this).val());
 
-            if (!totals[totalField]) {
-                totals[totalField] = 0;
-            }
-
             if (!isNaN(value)) {
-                totals[totalField] += value;
+                total += value;
             }
         });
 
+        return total;
+    }
+
+    function renderTotal(totalField) {
+        var formattedTotal = calculateTotalForField(totalField).toFixed(2);
+        $('[data-wl-bln-total-field="' + totalField + '"]').val(formattedTotal);
+        $('[data-wl-bln-total-display="' + totalField + '"]').text(formattedTotal);
+    }
+
+    function updateTotals() {
         $('[data-wl-bln-total-field]').each(function () {
-            var totalField = $(this).data('wl-bln-total-field');
-            var totalValue = totals[totalField] || 0;
-            var formattedTotal = totalValue.toFixed(2);
-            $(this).val(formattedTotal);
-            $('[data-wl-bln-total-display="' + totalField + '"]').text(formattedTotal);
+            renderTotal($(this).data('wl-bln-total-field'));
         });
     }
 
     updateTotals();
 
-    $(document).on('input change', '[data-wl-bln-component="1"]', function () {
-        updateTotals();
+    $(document).on('input change keyup', '[data-wl-bln-component="1"]', function () {
+        renderTotal($(this).data('wl-bln-total-target'));
     });
 });
