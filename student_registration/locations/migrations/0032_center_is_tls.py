a1e11361992f3685416989244ba9e79873ea0e55
Yosr Beydoun <ybaydoun@hotmail.com>
2026-07-03 12:48:05 +0300

    migration fix

--- student_registration/locations/migrations/0032_center_is_tls.py
+++ student_registration/locations/migrations/0032_center_is_tls.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('locations', '0031_center_active_during_emergency'),
+        ('locations', '0032_alter_center_active_during_emergency'),
     ]
 
     operations = [
