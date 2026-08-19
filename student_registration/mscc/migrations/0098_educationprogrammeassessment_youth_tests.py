43fbb345cdc5740e21c4e7b223b5a51cf8735e08
Yosr Beydoun <ybaydoun@hotmail.com>
2026-08-19 17:27:30 +0300

    migration simplify

--- student_registration/mscc/migrations/0098_educationprogrammeassessment_youth_tests.py
+++ student_registration/mscc/migrations/0098_educationprogrammeassessment_youth_tests.py
@@ -1,16 +1,6 @@
 from django.db import migrations, models
 
 
-def copy_youth_assessments(apps, schema_editor):
-    assessment_model = apps.get_model('mscc', 'EducationProgrammeAssessment')
-    youth_assessments = assessment_model.objects.filter(programme_type__startswith='Y')
-
-    for assessment in youth_assessments.iterator():
-        assessment.youth_pre_test = assessment.pre_test
-        assessment.youth_post_test = assessment.post_test
-        assessment.save(update_fields=['youth_pre_test', 'youth_post_test'])
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -27,6 +17,5 @@ class Migration(migrations.Migration):
             model_name='educationprogrammeassessment',
             name='youth_post_test',
             field=models.JSONField(default=dict),
-        ),
-        migrations.RunPython(copy_youth_assessments, migrations.RunPython.noop),
+        ), 
     ]
